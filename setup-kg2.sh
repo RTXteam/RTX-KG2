5f54927f9a997ec3bc66472199556a91e98ea1c5
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-11-21 10:33:20 -0800

    #301

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+BUILD_DIR=~/kg2-build
+VENV_DIR=~/kg2-venv
 sudo apt-get update
-sudo apt-get install -y python3-minimal python3-pip git
+sudo apt-get install -y python3-minimal python3-pip git default-jre
 sudo pip3 install virtualenv
-virtualenv ~/kg2-venv
-~/kg2-venv/bin/pip3 install ontobio
-mkdir -p ~/kg2-build
-cd ~/kg2-build && wget http://build.berkeleybop.org/userContent/owltools/owltools
-chmod a+x ~/kg2-build/owltools
+virtualenv ${VENV_DIR}
+${VENV_DIR}/bin/pip3 install ontobio
+mkdir -p ${BUILD_DIR}
+wget -P ${BUILD_DIR} http://build.berkeleybop.org/userContent/owltools/owltools
+chmod a+x ${BUILD_DIR}/owltools
