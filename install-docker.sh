cacdc2e40dfcb5384397832636ae5750ce170f5b
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-11-21 11:47:51 -0800

    #301

--- install-docker.sh
+++ install-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
