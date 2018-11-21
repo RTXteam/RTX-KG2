fd87e292df3aa45145a73655462d0731a83e29dd
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-11-21 10:13:55 -0800

    #301

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install -y python3-minimal python3-pip
+sudo apt-get install -y python3-minimal python3-pip git
 sudo pip3 install virtualenv
 virtualenv ~/kg2-venv
 ~/kg2-venv/bin/pip3 install ontobio
