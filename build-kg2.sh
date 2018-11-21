8945aa3bbcb4040f6481de1b5f2cd6b92c934c1c
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-11-21 10:33:58 -0800

    #301

--- build-kg2.sh
+++ build-kg2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/kg2-build
-~/kg2-venv/bin/python3 -u ~/kg2-code/build-kg2.py 2>~/kg2-build/stderr.log 1>~/kg2-build/stdout.log
+cd ~/kg2-build && ~/kg2-venv/bin/python3 -u build-kg2.py 2>stderr.log 1>stdout.log
