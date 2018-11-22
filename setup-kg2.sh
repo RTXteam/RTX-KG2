c87e2ef914ca58f6e5bb91c98431b1260979254c
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2018-11-21 21:53:15 -0800

    #301

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -9,6 +9,7 @@ sudo -H pip3 install virtualenv
 virtualenv ${VENV_DIR}
 ${VENV_DIR}/bin/pip3 install ontobio
 mkdir -p ${BUILD_DIR}
-cp ${CODE_DIR}/snomed.owl ${BUILD_DIR}/snomed.owl
+cp ${CODE_DIR}/snomed.owl ${BUILD_DIR}/
+cp ${CODE_DIR}/biolink-model--updated-for-kg2.yaml ${BUILD_DIR}/
 wget -P ${BUILD_DIR} http://build.berkeleybop.org/userContent/owltools/owltools
 chmod a+x ${BUILD_DIR}/owltools
