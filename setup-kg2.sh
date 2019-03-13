5395be5ed7dc9552378026c9fe0e174bb04d6267
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-12 21:38:44 -0700

    added code to install robot

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -16,8 +16,15 @@ sudo -H pip3 install virtualenv
 ## setup a python virtualenv for building KG2
 virtualenv ${VENV_DIR}
 
+## install robot
+wget https://github.com/ontodev/robot/releases/download/v1.3.0/robot.jar
+curl https://raw.githubusercontent.com/ontodev/robot/master/bin/robot > robot
+chmod a+x robot
+
+## we are not using pymongo but having it silences a runtime warning from ontobio:
+${VENV_DIR}/bin/pip3 install ontobio pymongo SNOMEDToOWL
+
 ## build OWL-XML representation of SNOMED CT
-${VENV_DIR}/bin/pip3 install SNOMEDToOWL
 unzip SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z.zip
 SNOMEDToOWL -f xml SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z/Snapshot SNOMEDToOWL/sct_core_us_gb.json -o snomed.owl
 ./robot relax --input snomed.owl --output snomed-relax.owl
@@ -27,8 +34,6 @@ ln -s ${CODE_DIR}/snomed-relax.owl ${BUILD_DIR}/
 ln -s ${CODE_DIR}/curies-to-categories.yaml ${BUILD_DIR}/
 ln -s ${CODE_DIR}/ontology-load-config.haml ${BUILD_DIR}/
 
-## we are not using pymongo but having it silences a runtime warning from ontobio:
-${VENV_DIR}/bin/pip3 install ontobio pymongo
 
 ## get owltools and set its permissions to executable
 wget -P ${BUILD_DIR} http://build.berkeleybop.org/userContent/owltools/owltools
