a60f36b2f13265f9d1ad0411bcb6b1f7924d6dac
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 13:27:12 -0700

    add code to only copy rsa public key if it is necessary

--- ec2-setup-remote-instance.sh
+++ ec2-setup-remote-instance.sh
@@ -5,20 +5,20 @@ INSTANCE_HOSTNAME=kg2.saramsey.org
 PUBLIC_KEY_FILE=id_rsa.pub
 AWS_PEM_FILE=/Volumes/WorkEncrypted/ramseyst-new-aws-login.pem
 
-# ## remove kg2.saramsey.org from the ~/.ssh/known_hosts file
-# ssh-keygen -R ${INSTANCE_HOSTNAME}
-
-# ## copy the id_rsa.pub file to the instance
-# scp -i ${AWS_PEM_FILE} \
-#     -o StrictHostKeyChecking=no \
-#     ~/.ssh/${PUBLIC_KEY_FILE} \
-#     ubuntu@${INSTANCE_HOSTNAME}:
-
-# ## append the id_rsa.pub file to the authorized_keys file
-# ssh -i /Volumes/WorkEncrypted/ramseyst-new-aws-login.pem \
-#     ubuntu@${INSTANCE_HOSTNAME} \
-#     'cat ${PUBLIC_KEY_FILE} >> ~/.ssh/authorized_keys && rm ${PUBLIC_KEY_FILE}'
-
+if ! ssh -q -o StrictHostKeyChecking=no ubuntu@${INSTANCE_HOSTNAME} exit; then
+    ## remove kg2.saramsey.org from the ~/.ssh/known_hosts file
+    ssh-keygen -R ${INSTANCE_HOSTNAME}
+    ## copy the id_rsa.pub file to the instance
+    scp -i ${AWS_PEM_FILE} \
+        -o StrictHostKeyChecking=no \
+        ~/.ssh/${PUBLIC_KEY_FILE} \
+        ubuntu@${INSTANCE_HOSTNAME}:
+    ## append the id_rsa.pub file to the authorized_keys file
+    ssh -i /Volumes/WorkEncrypted/ramseyst-new-aws-login.pem \
+        ubuntu@${INSTANCE_HOSTNAME} \
+        'cat ${PUBLIC_KEY_FILE} >> ~/.ssh/authorized_keys && rm ${PUBLIC_KEY_FILE}'
+fi
+    
 ## clone the RTX repo into the instance
 ssh ubuntu@${INSTANCE_HOSTNAME} git clone https://github.com/RTXteam/RTX.git
 
