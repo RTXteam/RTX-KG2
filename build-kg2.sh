3294cda515ae02b180440a26fe453b6e2c983a27
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 14:24:31 -0700

    use local copies of the config files in the build dir

--- build-kg2.sh
+++ build-kg2.sh
@@ -7,6 +7,6 @@ CODE_DIR=~/kg2-code
 
 export PATH=$PATH:${BUILD_DIR}
 cd ${BUILD_DIR} && ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/build-kg2.py \
-                              ${CODE_DIR}/curies-to-categories.yaml \
-                              ${CODE_DIR}/owl-load-inventory.yaml \
+                              ${BUILD_DIR}/curies-to-categories.yaml \
+                              ${BUILD_DIR}/owl-load-inventory.yaml \
                               2>stderr.log 1>stdout.log
