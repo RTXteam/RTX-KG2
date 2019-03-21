bee7fc9c4f291ac12ccd279a3e2d420b9090ae45
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 08:58:14 -0700

    updated

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -4,6 +4,7 @@
 BUILD_DIR=~/kg2-build
 VENV_DIR=~/kg2-venv
 CODE_DIR=~/kg2-code
+SNOMEDCT_FILE_BASE=SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z
 
 ## sym-link into RTX/code/kg2
 ln -s ~/RTX/code/kg2 ${CODE_DIR}
@@ -11,9 +12,11 @@ ln -s ~/RTX/code/kg2 ${CODE_DIR}
 ## install the Linux distro packages that we need
 sudo apt-get update
 sudo apt-get install -y python3-minimal python3-pip default-jre awscli
-sudo -H pip3 install virtualenv
 
-aws configure
+## this is for convenience when I am remote working
+sudo apt-get install -y emacs
+
+sudo -H pip3 install virtualenv
 
 ## setup a python virtualenv for building KG2
 virtualenv ${VENV_DIR}
@@ -30,9 +33,10 @@ mkdir -p ${BUILD_DIR}
 
 ## build OWL-XML representation of SNOMED CT
 cd ${BUILD_DIR}
-aws s3 cp s3://rtx-kg/SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z.zip .
-unzip SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z.zip
-SNOMEDToOWL -f xml SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z/Snapshot SNOMEDToOWL/sct_core_us_gb.json -o snomed.owl
+aws configure
+aws s3 cp s3://rtx-kg2/${SNOMEDCT_FILE_BASE}.zip .
+unzip ${SNOMEDCT_FILE_BASE}.zip
+SNOMEDToOWL -f xml ${SNOMEDCT_FILE_BASE}/Snapshot SNOMEDToOWL/sct_core_us_gb.json -o snomed.owl
 ./robot relax --input snomed.owl --output snomed-relax.owl
 
 ln -s ${CODE_DIR}/snomed-relax.owl ${BUILD_DIR}/
