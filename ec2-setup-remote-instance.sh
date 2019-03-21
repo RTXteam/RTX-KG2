39a871b909fa3dca11a3188738c6e3b380fd6be9
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 08:56:27 -0700

    updated

--- ec2-setup-remote-instance.sh
+++ ec2-setup-remote-instance.sh
@@ -12,9 +12,14 @@ scp -i ${AWS_PEM_FILE} \
     ~/.ssh/${PUBLIC_KEY_FILE} \
     ubuntu@${INSTANCE_HOSTNAME}:
 
+## append the id_rsa.pub file to the authorized_keys file
 ssh -i /Volumes/WorkEncrypted/ramseyst-new-aws-login.pem \
     ubuntu@${INSTANCE_HOSTNAME} \
     'cat ${PUBLIC_KEY_FILE} >> ~/.ssh/authorized_keys && rm ${PUBLIC_KEY_FILE}'
 
+## clone the RTX repo into the instance
 ssh ubuntu@${INSTANCE_HOSTNAME} git clone https://github.com/RTXteam/RTX.git
+
+## setup the instance using the setup script
+##ssh -t ubuntu@${INSTANCE_HOSTNAME} sh RTX/code/kg2/setup-kg2.sh
 
