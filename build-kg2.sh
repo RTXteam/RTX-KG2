1822d1fa3e0c41cbe60adb21cbebca9ff94d065c
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 14:09:39 -0700

    updated since build-kg2.py now uses argparse

--- build-kg2.sh
+++ build-kg2.sh
@@ -6,4 +6,7 @@ VENV_DIR=~/kg2-venv
 CODE_DIR=~/kg2-code
 
 export PATH=$PATH:${BUILD_DIR}
-cd ${BUILD_DIR} && ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/build-kg2.py 2>stderr.log 1>stdout.log
+cd ${BUILD_DIR} && ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/build-kg2.py \
+                              ${CODE_DIR}/curies-to-categories.yaml \
+                              ${CODE_DIR}/ontology-load-config.yaml \
+                              2>stderr.log 1>stdout.log
