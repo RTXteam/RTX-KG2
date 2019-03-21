696d9277bb12f2a7e5e9a2bff1b09302a54b928a
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 09:01:04 -0700

    need "zip" package

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -11,7 +11,7 @@ ln -s ~/RTX/code/kg2 ${CODE_DIR}
 
 ## install the Linux distro packages that we need
 sudo apt-get update
-sudo apt-get install -y python3-minimal python3-pip default-jre awscli
+sudo apt-get install -y python3-minimal python3-pip default-jre awscli zip
 
 ## this is for convenience when I am remote working
 sudo apt-get install -y emacs
