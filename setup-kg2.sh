61d1e824a69c5f8e2e6be9d7e78e97f8455e63ab
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 13:39:58 -0700

    refactoring

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -6,6 +6,8 @@ BUILD_DIR=~/kg2-build
 VENV_DIR=~/kg2-venv
 CODE_DIR=~/kg2-code
 SNOMEDCT_FILE_BASE=SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z
+S3_REGION=us-west-2
+S3_BUCKET=rtx-kg2
 
 ## sym-link into RTX/code/kg2
 ln -s ~/RTX/code/kg2 ${CODE_DIR}
@@ -45,12 +47,12 @@ wget -P ${BUILD_DIR} http://build.berkeleybop.org/userContent/owltools/owltools
 chmod a+x ${BUILD_DIR}/owltools
 
 ## build OWL-XML representation of SNOMED CT
-if ! aws s3 cp --region us-west-2 s3://rtx-kg2/test .; then
+if ! aws s3 cp --region ${S3_REGION} s3://${S3_BUCKET}/test .; then
     aws configure
 else
     rm test
 fi
-aws s3 cp --region us-west-2 s3://rtx-kg2/${SNOMEDCT_FILE_BASE}.zip ${BUILD_DIR}/
+aws s3 cp --region ${S3_REGION} s3://${S3_BUCKET}/${SNOMEDCT_FILE_BASE}.zip ${BUILD_DIR}/
 unzip ${BUILD_DIR}/${SNOMEDCT_FILE_BASE}.zip -d ${BUILD_DIR}
 ${VENV_DIR}/bin/SNOMEDToOWL -f xml ${BUILD_DIR}/${SNOMEDCT_FILE_BASE}/Snapshot \
            ${VENV_DIR}/lib/python3.6/site-packages/SNOMEDCTToOWL/conf/sct_core_us_gb.json \
