c11eda0e826cd632207c4e4bfa508079b5d4d3a0
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-21 09:34:19 -0700

    moved to setup script

--- make-snomed-owl.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# java -jar snomed-owl-toolkit-2.0.1-executable.jar rf2-to-owl -rf2-snapshot-archives SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z.zip
-unzip SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z.zip
-SNOMEDToOWL -f xml SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z/Snapshot SNOMEDToOWL/sct_core_us_gb.json -o snomed.owl
