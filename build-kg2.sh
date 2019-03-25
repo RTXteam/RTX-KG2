8755c53c70bf8de6f15794c8bc710501f17e0b43
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-25 16:24:47 -0700

    updated

--- build-kg2.sh
+++ build-kg2.sh
@@ -2,12 +2,12 @@
 set -euxo pipefail
 
 BUILD_DIR=~/kg2-build
-VENV_DIR=~/kg2-venv
-CODE_DIR=~/kg2-code
+source ${BUILD_DIR}/master-config.shinc
 
 export PATH=$PATH:${BUILD_DIR}
 cd ${BUILD_DIR} && ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/build-kg2.py \
                               curies-to-categories.yaml \
                               curies-to-urls-lookaside-list.yaml \
                               owl-load-inventory.yaml \
+                              ${SHARE_DIR} \
                               2>build-kg2-stderr.log 1>build-kg2-stdout.log
