8b4b590813ab57e8b860f7df02fe050aefef7154
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-24 21:22:05 -0700

    updated

--- setup-umls.sh
+++ setup-umls.sh
@@ -11,6 +11,7 @@ MYSQL_PASSWORD=1337
 MMSYS_DIR=${UMLS_DIR}/${UMLS_FILE_BASE}
 UMLS_RRDIST_DIR=${UMLS_DIR}
 UMLS_DEST_DIR=${UMLS_RRDIST_DIR}/METASUBSET
+MYSQL_CONF=${UMLS_DIR}/mysql-config.conf
 
 ## build UMLS
 
@@ -22,11 +23,6 @@ mkdir -p ${UMLS_DEST_DIR}
 aws s3 cp --region ${S3_REGION} s3://${S3_BUCKET}/umls-config.prop ${UMLS_DIR}/config.prop
 # unzip ${UMLS_DIR}/umls-${UMLS_FILE_BASE}.zip -d ${UMLS_DIR}/
 # unzip ${UMLS_DIR}/${UMLS_FILE_BASE}/mmsys.zip -d ${UMLS_DIR}/${UMLS_FILE_BASE}
-
-# sudo apt-get install -y mysql-server mysql-client
-sudo mysql -u root -e CREATE USER 'ubuntu'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'
-mysql -u ubuntu -p ${MYSQL_PASSWORD} -e CREATE DATABASE IF NOT EXISTS umls CHARACTER SET utf8 COLLATE utf8_unicode_ci
-mysql -u ubuntu -p ${MYSQL_PASSWORD} -e set global local_infile=1
 
 export METADIR=${UMLS_RRDIST_DIR}
 export DESTDIR=${UMLS_DEST_DIR}
@@ -44,4 +40,21 @@ ${JAVA_HOME}/bin/java -Djava.awt.headless=true \
                       -Doutput.uri=${DESTDIR} \
                       -Dmmsys.config.uri=${CONFIG_FILE} \
                       -Xms300M -Xmx1000M org.java.plugin.boot.Boot
+
+# sudo apt-get install -y mysql-server mysql-client
+sudo mysql -u root -e "CREATE USER 'ubuntu'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}'"
+sudo mysql -u root -e "GRANT ALL PRIVILEGES ON *.* to 'ubuntu'@'localhost'"
+echo <<EOF
+[client]
+user = ubuntu
+password = 1337
+host = 127.0.0.1
+EOF >> ${MYSQL_CONF}
+
+mysql --defaults-extra-file=${MYSQL_CONF} \
+      -e "CREATE DATABASE IF NOT EXISTS umls CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+
+mysql --defaults-extra-file=${MYSQL_CONF} \
+      -e "set global local_infile=1"
+
 
