7dbcebd0680d5c4c03d72aeaa76004ab6127acb9
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-25 16:11:07 -0700

    refactoring

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -3,18 +3,37 @@ set -euxo pipefail
 
 ## setup the shell variables for various directories
 BUILD_DIR=~/kg2-build
+
+## create the "build" directory where we will store the KG2 files:
+mkdir -p ${BUILD_DIR}
+
+cat >${BUILD_DIR}/master-config.shinc <<EOF
+BUILD_DIR=${BUILD_DIR}
 VENV_DIR=~/kg2-venv
 CODE_DIR=~/kg2-code
-SNOMEDCT_FILE_BASE=SnomedCT_USEditionRF2_PRODUCTION_20180901T120000Z
 S3_REGION=us-west-2
 S3_BUCKET=rtx-kg2
+EOF
+
+source ${BUILD_DIR}/master-config.shinc
 
 ## sym-link into RTX/code/kg2
 ln -s ~/RTX/code/kg2 ${CODE_DIR}
 
 ## install the Linux distro packages that we need
 sudo apt-get update
-sudo apt-get install -y python3-minimal python3-pip default-jre awscli zip curl
+sudo apt-get install -y python3-minimal \
+     python3-pip \
+     default-jre \
+     awscli \
+     zip \
+     curl \
+     wget \
+     flex \
+     bison \
+     libxml2-dev \
+     gtk-doc-tools \
+     libtool
 
 ## this is for convenience when I am remote working
 sudo apt-get install -y emacs
@@ -27,10 +46,7 @@ virtualenv ${VENV_DIR}
 
 ## Install python3 packages that we will need (Note: we are not using pymongo
 ## directly, but installing it silences a runtime warning from ontobio):
-${VENV_DIR}/bin/pip3 install ontobio pymongo SNOMEDToOWL
-
-## create the "build" directory where we will store the KG2 files:
-mkdir -p ${BUILD_DIR}
+${VENV_DIR}/bin/pip3 install ontobio pymongo
 
 ## make local copies of the config files in the build dir
 cp ${CODE_DIR}/curies-to-categories.yaml ${BUILD_DIR}
@@ -43,27 +59,25 @@ curl -LO -o ${BUILD_DIR}/robot.jar ${BUILD_DIR} https://github.com/RTXteam/robot
 curl https://raw.githubusercontent.com/RTXteam/robot/v1.3.0/bin/robot > ${BUILD_DIR}/robot
 chmod a+x ${BUILD_DIR}/robot
 
-## get owltools and set its permissions to executable
+## setup owltools
 curl -LO -o ${BUILD_DIR}/owltools https://github.com/RTXteam/owltools/releases/download/v0.3.0/owltools
-#wget -P ${BUILD_DIR} http://build.berkeleybop.org/userContent/owltools/owltools
 chmod a+x ${BUILD_DIR}/owltools
 
-## build OWL-XML representation of SNOMED CT
+## setup AWS CLI
 if ! aws s3 cp --region ${S3_REGION} s3://${S3_BUCKET}/test .; then
     aws configure
 else
     rm test
 fi
-aws s3 cp --region ${S3_REGION} s3://${S3_BUCKET}/${SNOMEDCT_FILE_BASE}.zip ${BUILD_DIR}/
-unzip ${BUILD_DIR}/${SNOMEDCT_FILE_BASE}.zip -d ${BUILD_DIR}
-${VENV_DIR}/bin/SNOMEDToOWL -f xml ${BUILD_DIR}/${SNOMEDCT_FILE_BASE}/Snapshot \
-           ${VENV_DIR}/lib/python3.6/site-packages/SNOMEDCTToOWL/conf/sct_core_us_gb.json \
-           -o ${BUILD_DIR}/snomed.owl
-${BUILD_DIR}/robot relax --input ${BUILD_DIR}/snomed.owl --output ${BUILD_DIR}/snomed-relax.owl
 
-# ## download Pickle ontology (DISABLED BECAUSE PICKLE ONTOLOGY NOT PARSABLE BY ONTOBIO)
-# wget -P ${BUILD_DIR} http://www.pickle.gr/Data/2.3/PICKLE2_3_OntologicalNetwork.zip
-# unzip ${BUILD_DIR}/PICKLE2_3_OntologicalNetwork.zip -d ${BUILD_DIR}
-# mv ${BUILD_DIR}/OntologicalNetwork.owl ${BUILD_DIR}/pickle.owl
-
-
+# setup raptor
+wget -P ${BUILD_DIR} http://download.librdf.org/source/raptor2-2.0.15.tar.gz
+tar xzf -C ${BUILD_DIR} ${BUILD_DIR}/raptor2-2.0.15.tar.gz
+cd ${BUILD_DIR}/raptor2-2.0.15
+./autogen.sh --prefix=/usr/local
+make
+make check
+sudo make install
+sudo ldconfig
+cd ${UMLS_DIR}/umls2rdf
+./checkOutputSyntax.sh
