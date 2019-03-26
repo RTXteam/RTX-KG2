42d5e5b22ca828a68a6e42a699a0621a9244177e
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-03-25 19:13:27 -0700

    fixing bugs

--- setup-kg2.sh
+++ setup-kg2.sh
@@ -63,12 +63,12 @@ cp ${CODE_DIR}/owl-load-inventory.yaml ${BUILD_DIR}
 
 ## install ROBOT (software: ROBOT is an OBO Tool) by downloading the jar file
 ## distribution and cURLing the startup script
-curl -LO -o ${BUILD_DIR}/robot.jar ${BUILD_DIR} https://github.com/RTXteam/robot/releases/download/v1.3.0/robot.jar
+curl -LO ${BUILD_DIR} https://github.com/RTXteam/robot/releases/download/v1.3.0/robot.jar > ${BUILD_DIR}/robot.jar 
 curl https://raw.githubusercontent.com/RTXteam/robot/v1.3.0/bin/robot > ${BUILD_DIR}/robot
 chmod a+x ${BUILD_DIR}/robot
 
 ## setup owltools
-curl -LO -o ${BUILD_DIR}/owltools https://github.com/RTXteam/owltools/releases/download/v0.3.0/owltools
+curl -LO https://github.com/RTXteam/owltools/releases/download/v0.3.0/owltools > ${BUILD_DIR}/owltools
 chmod a+x ${BUILD_DIR}/owltools
 
 ## setup AWS CLI
