10e334e364938dc94758f284518fe468810916a9
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-04-02 14:42:51 -0700

    added comments

--- build-kg2.sh
+++ build-kg2.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
 
+## load the master config file
 CONFIG_DIR=`dirname "$0"`
 source ${CONFIG_DIR}/master-config.shinc
 
+## set the path to include ${BUILD_DIR}
 export PATH=$PATH:${BUILD_DIR}
+
+## run the build-kg2.py script
 cd ${BUILD_DIR} && ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/build-kg2.py \
                               curies-to-categories.yaml \
                               curies-to-urls-lookaside-list.yaml \
