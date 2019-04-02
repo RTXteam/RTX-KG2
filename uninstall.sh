5368d747fdf57b7505dc35166490ae5185c2eab3
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-04-02 16:26:02 -0700

    removing redundant script

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-
-BUILD_DIR=~/kg2-build
-source ${BUILD_DIR}/master-config.shinc
-rm -r -f ${BUILD_DIR}
-rm -r -f ${VENV_DIR}
-rm -r -f ${CODE_DIR}
-rm -r -f ${SHARE_DIR}
