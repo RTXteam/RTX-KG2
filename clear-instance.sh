c1b5bcd7e23f255b814c9350b6ab9fdffd932f48
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-05-03 10:54:40 -0700

    fixing bug

--- clear-instance.sh
+++ clear-instance.sh
@@ -8,7 +8,8 @@ source ${CONFIG_DIR}/master-config.shinc
 read -p "Are you sure you are running this command in the KG2 build instance and not on your laptop? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
-then   
+then
+    cd ~
     rm -r -f ${BUILD_DIR}
     rm -r -f ${CODE_DIR}
     rm -r -f ${VENV_DIR}
