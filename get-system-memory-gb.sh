a4f386318971dc0cf563ce2f67a10ffcf1a0276e
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-05-03 09:53:11 -0700

    adding comment

--- get-system-memory-gb.sh
+++ get-system-memory-gb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-## estimate amount of system ram, in GB
+## estimate the amount of system ram, in GB (Linux-only script)
 MEM_BYTES=`cat /proc/meminfo | grep MemTotal | cut -f2 -d\: | cut -f1 -dk | sed 's/ //g'`
 DIVISOR=1048576
 MEM_GB=$((MEM_BYTES/DIVISOR))
