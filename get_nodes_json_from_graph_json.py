087d6ea304cdcf58bce0b3f986afc81260adbe0c
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-05-18 10:46:28 -0700

    #331

---
+++ get_nodes_json_from_graph_json.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+'''Extracts the nodes from a knowledge graph that is in JSON format; saves the nodes in JSON format.
+
+   Usage: get_nodes_json_from_graph_json.py <inputFile.json> <outputFile.json>
+'''
+
+__author__ = 'Stephen Ramsey'
+__copyright__ = 'Oregon State University'
+__credits__ = ['Stephen Ramsey']
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__maintainer__ = ''
+__email__ = ''
+__status__ = 'Prototype'
+
+import json
+import tempfile
+import argparse
+import shutil
+
+
+def make_arg_parser():
+    arg_parser = argparse.ArgumentParser(description='build-kg2: builds the KG2 knowledge graph for the RTX system')
+    arg_parser.add_argument('inputFile', type=str, nargs=1)
+    arg_parser.add_argument('outputFile', type=str, nargs=1)
+    return arg_parser
+
+
+if __name__ == "__main__":
+    args = make_arg_parser().parse_args()
+    temp_file = tempfile.mkstemp(prefix="kg2nodes")[1]
+    json.dump(json.load(open(args.inputFile, 'r'))['nodes'], open(temp_file, 'w'))
+    shutil.move(temp_file, args.outputFile)
