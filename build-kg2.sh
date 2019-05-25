db6846868e86f90513b58694143ea469031198e9
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-05-25 15:22:21 -0700

    #336

--- build-kg2.sh
+++ build-kg2.sh
@@ -9,7 +9,9 @@ source ${CONFIG_DIR}/master-config.shinc
 export PATH=$PATH:${BUILD_DIR}
 
 OUTPUT_FILE_BASE=kg2.json
+REPORT_FILE_BASE=kg2-report.json
 OUTPUT_FILE_FULL=${BUILD_DIR}/${OUTPUT_FILE_BASE}
+REPORT_FILE_FULL=${BUILD_DIR}/${REPORT_FILE_BASE}
 STDOUT_LOG_FILE=build-kg2-stdout.log
 STDERR_LOG_FILE=build-kg2-stderr.log
 OWL_LOAD_INVENTORY_FILE=${CODE_DIR}/owl-load-inventory.yaml
@@ -30,6 +32,9 @@ ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/build_kg2.py \
            2>${BUILD_DIR}/${STDERR_LOG_FILE} \
            1>${BUILD_DIR}/${STDOUT_LOG_FILE}
 
+${VENV_DIR}/bin/report_stats_on_kg.py \
+           ${OUTPUT_FILE_FULL} > ${REPORT_FILE_FULL}
+
 ## copy the KG to the public S3 bucket
 aws s3 cp --no-progress --region ${S3_REGION} ${OUTPUT_FILE_FULL} s3://${S3_BUCKET_PUBLIC}/
 
@@ -39,5 +44,7 @@ aws s3 cp --no-progress --region ${S3_REGION} ${BUILD_DIR}/${STDOUT_LOG_FILE} s3
 
 ## copy the config files to the public S3 bucket
 aws s3 cp --no-progress --region ${S3_REGION} ${OWL_LOAD_INVENTORY_FILE} s3://${S3_BUCKET_PUBLIC}/
+
+aws s3 cp --no-progress --regino ${S3_REGION} ${REPORT_FILE_FULL} s3://${S3_BUCKET_PUBLIC}
 
 echo "================= script finished ================="
