58b73c259ecce5ae8b438d7a22b0783e748d7642
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-06-03 21:26:30 -0700

    #363

--- semmeddb_mysql_to_json.py
+++ semmeddb_mysql_to_json.py
@@ -25,6 +25,44 @@ import tempfile
 SEMMEDDB_IRI = 'https://skr3.nlm.nih.gov/SemMedDB'
 
 
+def make_rel(preds_dict: dict,
+             subject_curie: str,
+             object_curie: str,
+             predicate: str,
+             pmid: str,
+             pub_date: str,
+             sentence: str,
+             subject_score: str,
+             object_score: str):
+    key = subject_curie + '-' + predicate + '-' + object_curie
+    key_val = preds_dict.get(key, None)
+    publication_curie = 'PMID:' + pmid
+    publication_info_dict = {
+        'publication date': pub_date,
+        'sentence': sentence,
+        'subject score': subject_score,
+        'object score': object_score}
+    if key_val is None:
+        relation_type = predicate.replace('_', ' ').lower()
+        relation_iri = relation_type.title().replace(' ', '')
+        relation_iri = relation_iri[0].lower() + relation_iri[1:]
+        relation_iri = SEMMEDDB_IRI + '#' + relation_iri
+        key_val = {'subject': subject_curie,
+                   'object': object_curie,
+                   'type': relation_type,
+                   'relation': relation_iri,
+                   'relation curie': 'SEMMEDDB:' + relation_type,
+                   'negated': False,
+                   'publications': [publication_curie],
+                   'publications info': {publication_curie: publication_info_dict},
+                   'update date': kg2_util.format_timestamp(curr_timestamp.timetuple()),
+                   'provided by': SEMMEDDB_IRI}
+        preds_dict[key] = key_val
+    else:
+        key_val['publications info'][publication_curie] = publication_info_dict
+        key_val['publications'] = key_val['publications'] + [publication_curie]
+
+        
 def make_arg_parser():
     arg_parser = argparse.ArgumentParser(description='semmeddb_mysql_to_json.py: extracts all the predicate triples from SemMedDB, in the RTX KG2 JSON format')
     arg_parser.add_argument('--test', dest='test', action="store_true", default=False)
@@ -51,35 +89,26 @@ if __name__ == '__main__':
         results = cursor.fetchall()
         for (pmid, subject_cui_str, predicate, object_cui_str, pub_date, sentence,
              subject_score, object_score, curr_timestamp) in results:
-            for subject_cui in subject_cui_str.split("|"):
-                for object_cui in object_cui_str.split("|"):
-                    key = subject_cui + '-' + predicate + '-' + object_cui
-                    key_val = preds_dict.get(key, None)
-                    publication_curie = 'PMID:' + pmid
-                    publication_info_dict = {
-                        'publication date': pub_date,
-                        'sentence': sentence,
-                        'subject score': subject_score,
-                        'object score': object_score}
-                    if key_val is None:
-                        relation_type = predicate.replace('_', ' ').lower()
-                        relation_iri = relation_type.title().replace(' ', '')
-                        relation_iri = relation_iri[0].lower() + relation_iri[1:]
-                        relation_iri = SEMMEDDB_IRI + '#' + relation_iri
-                        key_val = {'subject': 'CUI:' + subject_cui,
-                                   'object': 'CUI:' + object_cui,
-                                   'type': relation_type,
-                                   'relation': relation_iri,
-                                   'relation curie': 'SEMMEDDB:' + relation_type,
-                                   'negated': False,
-                                   'publications': [publication_curie],
-                                   'publications info': {publication_curie: publication_info_dict},
-                                   'update date': kg2_util.format_timestamp(curr_timestamp.timetuple()),
-                                   'provided by': SEMMEDDB_IRI}
-                        preds_dict[key] = key_val
-                    else:
-                        key_val['publications info'][publication_curie] = publication_info_dict
-                        key_val['publications'] = key_val['publications'] + [publication_curie]
+            subject_cui_split = subject_cui_str.split("|")
+            subject_cui = subject_cui_split[0]
+            if len(subject_cui_split) > 1:
+                subject_entrez_id = subject_cui_split[1]
+            else:
+                subject_entrez_id = None
+            object_cui_split = object_cui_str.split("|")
+            object_cui = object_cui_split[0]
+            if len(object_cui_split) > 1:
+                object_entrez_id = object_cui_split[1]
+            else:
+                object_entrez_id = None
+            make_rel(preds_dict, 'CUI:' + subject_cui, 'CUI:' + object_cui, predicate, pmid,
+                     pub_date, sentence, subject_score, object_score)
+            if subject_entrez_id is not None:
+                make_rel(preds_dict, 'NCBIGene:' + subject_entrez_id, 'CUI:' + object_cui,
+                         predicate, pmid, pub_date, sentence, subject_score, object_score)
+            if object_entrez_id is not None:
+                make_rel(preds_dict, 'CUI:' + subject_cui, 'NCBIGene:' + object_entrez_id,
+                         predicate, pmid, pub_date, sentence, subject_score, object_score)
     connection.close()
     out_graph = {'edges': [rel_dict for rel_dict in preds_dict.values()],
                  'nodes': []}
