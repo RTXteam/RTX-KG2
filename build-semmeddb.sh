4e0cc5ba01fdee7c23464d0786af792567ca8104
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-06-05 11:33:34 -0700

    #366

--- build-semmeddb.sh
+++ build-semmeddb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
-# Usage: build-semmeddb.sh <output_file.json> [test]
+# Usage: build-semmeddb.sh <output_file.json> [test|extract]
 
 CONFIG_DIR=`dirname "$0"`
 source ${CONFIG_DIR}/master-config.shinc
@@ -16,19 +16,23 @@ SEMMED_DIR=${BUILD_DIR}/semmeddb
 SEMMED_SQL_FILE=semmed${SEMMED_VER}_R_WHOLEDB_${SEMMED_DATE}.sql
 MYSQL_DBNAME=semmeddb
 
-mkdir -p ${SEMMED_DIR}
+if [ "${BUILD_FLAG}" == "" ]
+then
+    
+    mkdir -p ${SEMMED_DIR}
 
 ## estimate amount of system ram, in GB
-MEM_GB=`${CODE_DIR}/get-system-memory-gb.sh`
+    MEM_GB=`${CODE_DIR}/get-system-memory-gb.sh`
 
-aws s3 cp --no-progress --region ${S3_REGION} s3://${S3_BUCKET}/${SEMMED_SQL_FILE}.gz ${SEMMED_DIR}/
-gunzip ${SEMMED_DIR}/${SEMMED_SQL_FILE}.gz
+    aws s3 cp --no-progress --region ${S3_REGION} s3://${S3_BUCKET}/${SEMMED_SQL_FILE}.gz ${SEMMED_DIR}/
+    gunzip ${SEMMED_DIR}/${SEMMED_SQL_FILE}.gz
 
 ## create the "umls" database
-mysql --defaults-extra-file=${MYSQL_CONF} \
-      -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DBNAME} CHARACTER SET utf8 COLLATE utf8_unicode_ci"
+    mysql --defaults-extra-file=${MYSQL_CONF} \
+          -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DBNAME} CHARACTER SET utf8 COLLATE utf8_unicode_ci"
 
-mysql --defaults-extra-file=${MYSQL_CONF} --database=${MYSQL_DBNAME} < ${SEMMED_DIR}/${SEMMED_SQL_FILE}
+    mysql --defaults-extra-file=${MYSQL_CONF} --database=${MYSQL_DBNAME} < ${SEMMED_DIR}/${SEMMED_SQL_FILE}
+fi
 
 if [ "${BUILD_FLAG}" == 'test' ]
 then
