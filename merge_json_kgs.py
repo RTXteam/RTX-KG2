65a638bbe6ee0ff34e9754ce367425f33a91bfeb
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-06-14 10:13:26 -0700

    #390

--- merge_json_kgs.py
+++ merge_json_kgs.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 '''kg2_merge.py: merge two KGs that are in the KG2 JSON format
 
-   Usage: kg2_merge.py <kg_alpha.json> <kg_beta.json> <output.json>
-   Note: any of the three file arguments can have a ".gz" extension, in which case gzip is used for reading/writing.
-   (WARNING: using the gzip option significantly increases transient memory usage)
+   Usage: kg2_merge.py [--test] --inputFiles <file1> <file2> ... <fileN> --outputFile <outputFile>
 '''
 
 __author__ = 'Stephen Ramsey'
@@ -16,48 +14,32 @@ __email__ = ''
 __status__ = 'Prototype'
 
 import argparse
-import gzip
 import json
 import kg2_util
-import shutil
-import tempfile
 
 
 def make_arg_parser():
     arg_parser = argparse.ArgumentParser(description='semmeddb_mysql_to_json.py: extracts all the predicate triples from SemMedDB, in the RTX KG2 JSON format')
-    arg_parser.add_argument('kgAlpha', type=str, nargs=1)
-    arg_parser.add_argument('kgBeta', type=str, nargs=1)
-    arg_parser.add_argument('outputFile', type=str, nargs=1)
+    arg_parser.add_argument('--test', dest='test', action='store_true', default=False)
+    arg_parser.add_argument('--inputFiles', type=str, nargs='+')
+    arg_parser.add_argument('--outputFile', type=str, nargs=1)
     return arg_parser
+
 
 if __name__ == '__main__':
     args = make_arg_parser().parse_args()
-    kg1_file_name = args.kgAlpha[0]
-    if not kg1_file_name.endswith('.gz'):
-        kg1_file = open(kg1_file_name, 'r')
-        kg1 = json.load(kg1_file)
-    else:
-        kg1_file = gzip.GzipFile(kg1_file_name, 'r')
-        kg1 = json.loads(kg1_file.read().decode('utf-8'))
-    kg2_file_name = args.kgBeta[0]
-    if not kg2_file_name.endswith('.gz'):
-        kg2_file = open(kg2_file_name, 'r')
-        kg2 = json.load(kg2_file)
-    else:
-        kg2_file = gzip.GzipFile(kg2_file_name, 'r')
-        kg2 = json.loads(kg2_file.read().decode('utf-8'))
-    for node in kg2['nodes']:
-        if node in kg1['nodes']:
+    nodes = {}
+    edges = []
+    for input_file_name in args.inputFiles:
+        kg_file = open(input_file_name, 'r')
+        input_kg = json.load(kg_file)
+        for node in input_kg['nodes']:
             node_id = node['id']
-            kg1_node = kg1['nodes'][node_id]
-            kg1[node_id] = kg2_util.merge_two_dicts(kg1_node, node)
-    kg1['edges'] = kg1['edges'] + kg2['edges']
-    temp_output_file_name = tempfile.mkstemp(prefix='kg2-')[1]
+            if node_id in nodes:
+                nodes[node_id] = kg2_util.merge_two_dicts(nodes[node_id], node)
+        edges += input_kg['edges']
+    output_kg = {'nodes': [node for node in nodes.values()],
+                 'edges': edges}
     output_file_name = args.outputFile[0]
-    if not output_file_name.endswith('.gz'):
-        temp_output_file = open(temp_output_file_name, 'w')
-        json.dump(kg1, temp_output_file, indent=4, sort_keys=True)
-    else:
-        temp_output_file = gzip.GzipFile(temp_output_file_name, 'w')
-        temp_output_file.write(json.dumps(kg1, indent=4, sort_keys=True).encode('utf-8'))
-    shutil.move(temp_output_file_name, output_file_name)
+    test_mode = args.test
+    kg2_util.save_json(output_kg, output_file_name, test_mode)
