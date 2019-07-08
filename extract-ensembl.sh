cb04fd54946c0aeab734b4d1828a20ba11269481
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-07-08 14:43:34 -0700

    #406

---
+++ extract-ensembl.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# extract-ensembl.sh: download Ensembl datafile of annotated human genes 
+# Copyright 2019 Stephen A. Ramsey <stephen.ramsey@oregonstate.edu>
+
+set -o nounset -o pipefail -o errexit
+
+if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
+    echo Usage: "$0 [all|test]"
+    exit 2
+fi
+
+echo "================= starting build-uniprotkb.sh ================="
+date
+
+CONFIG_DIR=`dirname "$0"`
+source ${CONFIG_DIR}/master-config.shinc
+
+ENSEMBL_RELEASE=97
+
+## supply a default value for the BUILD_FLAG string
+BUILD_FLAG=${1:-""}
+
+
+ENSEMBL_JSON_FILE=${1:-"${BUILD_DIR}/ensembl_genes_homo_sapiens.json"}
+OUTPUT_DIR=`dirname ${ENSEMBL_JSON_FILE}`
+
+mkdir -p ${OUTPUT_DIR}
+
+CURL_GET="curl -s -L"
+
+${CURL_GET} ftp://ftp.ensembl.org/pub/release-${ENSEMBL_RELEASE}/json/homo_sapiens/homo_sapiens.json > ${ENSEMBL_JSON_FILE}
+
