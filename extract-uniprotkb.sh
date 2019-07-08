86519a7d9cd990212a4da9e457b602163ff7ebe9
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-07-08 14:44:21 -0700

    #406 #401

--- extract-uniprotkb.sh
+++ extract-uniprotkb.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
-# build-uniprotkb.sh: download UniProtKB dat distribution and extract to a dat file
+# extract-uniprotkb.sh: download UniProtKB dat distribution and extract to a dat file
 # Copyright 2019 Stephen A. Ramsey <stephen.ramsey@oregonstate.edu>
 
 set -o nounset -o pipefail -o errexit
 
 if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
-    echo Usage: "$0 [all|test]"
+    echo Usage: "$0 <uniprot_output_file.dat>"
     exit 2
 fi
 
-echo "================= starting build-uniprotkb.sh ================="
+echo "================= starting extract-uniprotkb.sh ================="
 date
 
 CONFIG_DIR=`dirname "$0"`
@@ -17,10 +17,7 @@ source ${CONFIG_DIR}/master-config.shinc
 
 UNIPROTKB_DAT_FILE=${1:-"${BUILD_DIR}/uniprot_sprot.dat"}
 
-## supply a default value for the BUILD_FLAG string
-BUILD_FLAG=${1:-""}
-
-UNIPROTKB_DIR=${BUILD_DIR}/uniprotkb
+UNIPROTKB_DIR=`dirname ${UNIPROTKB_DAT_FILE}`
 CURL_GET="curl -s -L"
 
 mkdir -p ${UNIPROTKB_DIR}
@@ -28,7 +25,7 @@ ${CURL_GET} ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledg
             > ${UNIPROTKB_DIR}/uniprot_sprot.dat.gz
 
 zcat ${UNIPROTKB_DIR}/uniprot_sprot.dat.gz > /tmp/uniprot_sprot.dat
-mv /tmp/uniprot_sprott.dat ${UNIPROTKB_DAT_FILE}
+mv /tmp/uniprot_sprot.dat ${UNIPROTKB_DAT_FILE}
 
 date
 echo "================= script finished ================="
