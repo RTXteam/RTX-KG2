127f139ada4265d98dfddeff0d353229f1f3067d
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-07-10 18:03:28 -0700

    #412

--- extract-dgidb.sh
+++ extract-dgidb.sh
@@ -5,11 +5,11 @@
 set -o nounset -o pipefail -o errexit
 
 if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
-    echo Usage: "$0 <output_tsv_file>"
+    echo Usage: "$0 <output_tsv_dir>"
     exit 2
 fi
 
-OUTPUT_TSV_FILE=${1:-"${BUILD_DIR}/dgidb/interations.tsv"}
+DGIDB_DIR=${1:-"${BUILD_DIR}/dgidb/"}
 
 echo "================= starting build-unichem.sh ================="
 date
@@ -17,11 +17,10 @@ date
 CONFIG_DIR=`dirname "$0"`
 source ${CONFIG_DIR}/master-config.shinc
 
-DGIDB_DIR=`dirname ${OUTPUT_TSV_FILE}`
-
 mkdir -p ${DGIDB_DIR}
 
-${CURL_GET} http://www.dgidb.org/data/interactions.tsv > ${OUTPUT_TSV_FILE}
+${CURL_GET} http://www.dgidb.org/data/interactions.tsv > ${DGIDB_DIR}/interactions.tsv
+${CURL_GET} http://www.dgidb.org/data/genes.tsv > ${DGIDB_DIR}/genes.tsv
 
 date
 echo "================= script finished ================="
