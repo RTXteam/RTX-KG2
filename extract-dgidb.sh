c38cfb3f707a5d5ece73b4ebe7fad2b9147f36eb
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-07-10 17:40:33 -0700

    #412

---
+++ extract-dgidb.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# extract-dgidb.sh: download DGIDB interactions dataset to local TSV file
+# Copyright 2019 Stephen A. Ramsey <stephen.ramsey@oregonstate.edu>
+
+set -o nounset -o pipefail -o errexit
+
+if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
+    echo Usage: "$0 <output_tsv_file>"
+    exit 2
+fi
+
+OUTPUT_TSV_FILE=${1:-"${BUILD_DIR}/dgidb/interations.tsv"}
+
+echo "================= starting build-unichem.sh ================="
+date
+
+DGIDB_DIR=`dirname ${OUTPUT_TSV_FILE}`
+
+mkdir -p ${DGIDB_DIR}
+
+${CURL_GET} http://www.dgidb.org/data/interactions.tsv > ${OUTPUT_TSV_FILE}
+
+date
+echo "================= script finished ================="
