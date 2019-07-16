4d8954933d5ecea49d0434a422e9d7cce506cdbf
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-07-16 09:34:19 -0700

    #349

--- install-neo4j.sh
+++ install-neo4j.sh
@@ -16,3 +16,10 @@ echo 'deb http://debian.neo4j.org/repo stable/' > neo4j.list
 sudo mv neo4j.list /etc/apt/sources.list.d/neo4j.list
 sudo apt-get update -y
 sudo apt-get install -y neo4j
+sudo cp /etc/neo4j/neo4j.conf /etc/neo4j/neo4j.conf.ori
+cat /etc/neo4j/neo4j.conf.ori | sed 's/#dbms.connectors.default_listen_address/dbms.connectors.default_listen_address/g' > /tmp/neo4j.conf
+sudo service neo4j stop
+sudo mv /tmp/neo4j.conf /etc/neo4j
+sudo service neo4j start
+
+echo "Now you need to go to http://<hostname>:7474 in your browser and set a default password for the database"
