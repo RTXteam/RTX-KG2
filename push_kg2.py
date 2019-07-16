2c9f3ed03b8e1068e4e036ad9f0a1a659ad02e18
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-07-16 09:45:32 -0700

    #349

--- push_kg2.py
+++ push_kg2.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+'''push_kg2.py: push a JSON KG to an (empty) Neo4j database
+
+   Usage: push_kg2.py 
+'''
+
 import os
 import sys
-from collections import namedtuple, Counter
 from neo4j.v1 import GraphDatabase, basic_auth
 import argparse
 import time
@@ -14,7 +19,7 @@ except ImportError:
 
 
 class push_kg2:
-    
+
     def __init__(self, bolt, user = None, password = None):
         """
         :param bolt: A string containing the bolt address of the neo4j instance you wish to upload to
@@ -37,7 +42,7 @@ class push_kg2:
         """
         self.driver.close()
 
-    def push_nodes(self, json_file, batch = 10000):
+    def push_nodes(self, json_file, batch=10000):
         """
         :param json_file: A string containing the path (or URL) to the json file
         :param batch: A string or integer containing a positive integer number of nodes to include in each batch upload
@@ -46,28 +51,28 @@ class push_kg2:
             batch = str(batch)
         # This need to be updated with publications info when the json is updated to be a string
         cypher_upload_nodes = 'CALL apoc.periodic.iterate("' +\
-                                "WITH '" + json_file + "' AS file " +\
-                                'CALL apoc.load.json(file) YIELD value ' +\
-                                'UNWIND value.nodes AS n RETURN n ' +\
-                                '"," ' +\
-                                "CALL apoc.create.node([n.`category label`, 'Base'], {}) YIELD node " +\
-                                'SET node.category = n.category, node.id = n.id, node.name = n.name, ' +\
-                                'node.`creation date` = n.`creation date`, node.deprecated = n.deprecated, ' +\
-                                'node.description = n.description, node.`full name` = n.`full name`, ' +\
-                                'node.iri = n.iri, node.`ontology node type` = n.`ontology node type`, ' +\
-                                'node.`provided by` = n.`provided by`, node.publications = n.publications, ' +\
-                                'node.`replaced by` = n.`replaced by`, node.synonym = n.synonym, ' +\
-                                'node.`update date` = n.`update date`, ' +\
-                                'node.`category label` = n.`category label` ' +\
-                                'RETURN count(*)' +\
-                                '", {batchSize: ' + batch + ', iterateList: true})'
-                                #Redundancy to make querying by property rather than label possible
+            "WITH '" + json_file + "' AS file " +\
+            'CALL apoc.load.json(file) YIELD value ' +\
+            'UNWIND value.nodes AS n RETURN n ' +\
+            '"," ' +\
+            "CALL apoc.create.node([n.`category label`, 'Base'], {}) YIELD node " +\
+            'SET node.category = n.category, node.id = n.id, node.name = n.name, ' +\
+            'node.`creation date` = n.`creation date`, node.deprecated = n.deprecated, ' +\
+            'node.description = n.description, node.`full name` = n.`full name`, ' +\
+            'node.iri = n.iri, node.`ontology node type` = n.`ontology node type`, ' +\
+            'node.`provided by` = n.`provided by`, node.publications = n.publications, ' +\
+            'node.`replaced by` = n.`replaced by`, node.synonym = n.synonym, ' +\
+            'node.`update date` = n.`update date`, ' +\
+            'node.`category label` = n.`category label` ' +\
+            'RETURN count(*)' +\
+            '", {batchSize: ' + batch + ', iterateList: true})'
+        # Redundancy to make querying by property rather than label possible
         with self.driver.session() as session:
             session.run("CREATE CONSTRAINT ON (n:Base) ASSERT n.id IS UNIQUE")
             res = session.run(cypher_upload_nodes)
         return res.value()[0]
 
-    def push_edges(self, json_file, batch = 10000):
+    def push_edges(self, json_file, batch=10000):
         """
         :param json_file: A string containing the path (or URL) to the json file
         :param batch: A string or integer containing a positive integer number of edges to include in each batch upload
@@ -75,31 +80,35 @@ class push_kg2:
         if isinstance(batch, int):
             batch = str(batch)
         cypher_upload_edges = 'CALL apoc.periodic.iterate("' +\
-                                "WITH '" + json_file + "' AS file " +\
-                                'CALL apoc.load.json(file) YIELD value ' +\
-                                'UNWIND value.edges AS e RETURN e ' +\
-                                '"," ' +\
-                                'MATCH (a:Base {id:e.subject}) , (b:Base {id:e.object}) ' +\
-                                'CALL apoc.create.relationship(a,e.`edge label`,{}, b) YIELD rel ' +\
-                                'SET rel.`provided by` = e.`provided by`, rel.publications = e.publications, ' +\
-                                'rel.negated = e.negated, rel.relation = e.relation, rel.`relation curie` = e.`relation curie`, ' +\
-                                'rel.`update date` = e.`update date`, ' +\
-                                'rel.`edge label` = e.`edge label` ' +\
-                                'RETURN count(*) ' +\
-                                '", {batchSize: ' + batch + ', iterateList: true})'
-        #Redundancy to make querying by property rather than label possible
+            "WITH '" + json_file + "' AS file " +\
+            'CALL apoc.load.json(file) YIELD value ' +\
+            'UNWIND value.edges AS e RETURN e ' +\
+            '"," ' +\
+            'MATCH (a:Base {id:e.subject}) , (b:Base {id:e.object}) ' +\
+            'CALL apoc.create.relationship(a,e.`edge label`,{}, b) YIELD rel ' +\
+            'SET rel.`provided by` = e.`provided by`, rel.publications = e.publications, ' +\
+            'rel.negated = e.negated, rel.relation = e.relation, rel.`relation curie` = e.`relation curie`, ' +\
+            'rel.`update date` = e.`update date`, ' +\
+            'rel.`edge label` = e.`edge label` ' +\
+            'RETURN count(*) ' +\
+            '", {batchSize: ' + batch + ', iterateList: true})'
+        # Redundancy to make querying by property rather than label possible
         with self.driver.session() as session:
             res = session.run(cypher_upload_edges)
         return res.value()[0]
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-u", "--user", type=str, help="The neo4j username", default = None)
-    parser.add_argument("-p", "--password", type=str, help="The neo4j passworl", default = None)
-    parser.add_argument("-b", "--bolt", type=str, help="The neo4j bolt address", default = "bolt://kg2ase2.rtx.ai:7687")
-    parser.add_argument("-f", "--file", type=str, help="The path of the json file for upload prefixed with 'file:///' (can also be a url)", default = "file:///var/lib/neo4j/import/kg2-test2.json")
-    parser.add_argument("-n","--nodes", action="store_true", help="include if you just want to upload nodes (if used in conjunction with edges option will upload both)")
-    parser.add_argument("-e","--edges", action="store_true", help="include if you just want to upload edges (if used in conjunction with nodes option will upload both)")
+    parser.add_argument("-u", "--user", type=str, help="The neo4j username", default=None)
+    parser.add_argument("-p", "--password", type=str, help="The neo4j passworl", default=None)
+    parser.add_argument("-b", "--bolt", type=str, help="The neo4j bolt address", default="bolt://kg2ase2.rtx.ai:7687")
+    parser.add_argument("-f", "--file", type=str, help="The path of the json file for upload prefixed with 'file:///' (can also be a url)",
+                        defaul="file:///var/lib/neo4j/import/kg2-test2.json")
+    parser.add_argument("-n", "--nodes", action="store_true",
+                        help="include if you just want to upload nodes (if used in conjunction with edges option will upload both)")
+    parser.add_argument("-e", "--edges", action="store_true",
+                        help="include if you just want to upload edges (if used in conjunction with nodes option will upload both)")
     parser.add_argument("--batch", type=int, help="The batch size used for uploading the edges (must be a positive integer)", default=10000)
     parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
@@ -113,7 +122,7 @@ if __name__ == "__main__":
 
     if args.bolt is None:
         raise Exception("Must specify a bolt address for the neo4j instance.")
-    
+
     node_flag = True
     edge_flag = True
     if args.nodes and not args.edges:
@@ -126,13 +135,12 @@ if __name__ == "__main__":
         t0 = time.time()
         count = kg2_pusher.push_nodes(args.file)
         if args.debug:
-            print(str(count), " batches of ", args.batch," nodes uploaded in ", str(time.time() - t0), " seconds.")
+            print(str(count), " batches of ", args.batch, " nodes uploaded in ", str(time.time() - t0), " seconds.")
     if edge_flag:
         t0 = time.time()
         count = kg2_pusher.push_edges(args.file, str(args.batch))
         if args.debug:
-            print(str(count), " batches of ", args.batch," edges uploaded in ", str(time.time() - t0), " seconds.")
+            print(str(count), " batches of ", args.batch, " edges uploaded in ", str(time.time() - t0), " seconds.")
 
     kg2_pusher.close()
-
 
