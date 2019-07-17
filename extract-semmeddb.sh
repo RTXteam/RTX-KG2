9e172efa991ee4296463be4e01030bfab8edf157
Erica Wood <36611732+BluePiggy49@users.noreply.github.com>
2019-07-17 15:28:46 -0700

    Update extract-semmeddb.sh

--- extract-semmeddb.sh
+++ extract-semmeddb.sh
@@ -5,7 +5,7 @@
 set -o nounset -o pipefail -o errexit
 
 if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
-    echo Usage: "$0 <output_file.json> [all]"
+    echo Usage: "$0 <output_file.json> [test]"
     exit 2
 fi
 
