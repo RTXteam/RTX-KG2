3660c2c8399b6d111288017a510f962f2d086a53
Erica Wood <36611732+BluePiggy49@users.noreply.github.com>
2019-08-01 11:07:17 -0700

    #430

--- tsv_to_neo4j.sh
+++ tsv_to_neo4j.sh
@@ -39,7 +39,7 @@ sudo rm -rf ${DATABASE_PATH}/databases/${DATABASE}
 # import TSV files into Neo4j as Neo4j
 sudo -u neo4j neo4j-admin import --nodes "${TSV_DIR}/nodes_header.tsv,${TSV_DIR}/nodes.tsv" \
     --relationships "${TSV_DIR}/edges_header.tsv,${TSV_DIR}/edges.tsv" \
-    --max-memory=5G --multiline-fields=true --delimiter "\009" \
+    --max-memory=90G --multiline-fields=true --delimiter "\009" \
     --report-file="${TSV_DIR}/import.report" --database=${DATABASE} --ignore-missing-nodes=true
 
 # start Neo4j database up for use
