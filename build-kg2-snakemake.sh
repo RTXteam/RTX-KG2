990ce5e123ee8ca234322bf5940f9dd923f2db8b
Erica Wood <36611732+BluePiggy49@users.noreply.github.com>
2019-08-14 12:01:08 -0700

    #338

--- build-kg2-snakemake.sh
+++ build-kg2-snakemake.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# build-kg2-snakemake.sh: Create KG2 JSON file from scratch
+# build-kg2-snakemake.sh: Create KG2 JSON file from scratch using snakemake
 # Copyright 2019 Stephen A. Ramsey
 # Author Erica C. Wood
 
@@ -23,8 +23,11 @@ BUILD_FLAG=${1-""}
 
 if [[ "${BUILD_FLAG}" == "test" ]]
 then
+    # The test argument for bash scripts (ex. extract-semmeddb.sh test)
     TEST_ARG="test"
+    # The test argument for file names (ex. kg2-owl-test.json)
     TEST_ARG_D="-test"
+    # The test argument for python scripts (ex. python3 uniprotkb_dat_to_json.py --test)
     TEST_ARG_DD="--test"
 else
     TEST_ARG=""
@@ -32,13 +35,13 @@ else
     TEST_ARG_DD=""
 fi
 
-# change into the root director
-cd ~
-
-# activate the virtualenv and start running snakemake with the Snakefile in kg2-code
-source ${VENV_DIR}/bin/activate
-snakemake --snakefile /home/ubuntu/kg2-code/Snakefile -R Finish -j --config test="${TEST_ARG}" testd="${TEST_ARG_D}" testdd="${TEST_ARG_DD}"
+# Change directories into the home directory because snakemake doesn't like ~ in input/output names, so paths are respective
+# Run snakemake from the virtualenv but run the snakefile in kg2-code
+# -R Finish: Run all of the rules in the snakefile
+# -j: Run the rules in parallel
+# -config: give the test arguments to the snakefile
+cd ~ && ${VENV_DIR}/bin/snakemake --snakefile /home/ubuntu/kg2-code/Snakefile -R Finish -j --config test="${TEST_ARG}" testd="${TEST_ARG_D}" testdd="${TEST_ARG_DD}"
 
 date
 echo "================ script finished ============================"
-} >~/snakemake.log 2>&1
+} >${BUILD_DIR}/build-kg2-snakemake.log 2>&1
