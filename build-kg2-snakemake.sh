e0782eb217f5eebe3f17c2d709956d50f8319eb4
Erica Wood <36611732+BluePiggy49@users.noreply.github.com>
2019-08-14 11:23:47 -0700

    #338

---
+++ build-kg2-snakemake.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# build-kg2-snakemake.sh: Create KG2 JSON file from scratch
+# Copyright 2019 Stephen A. Ramsey
+# Author Erica C. Wood
+
+set -o nounset -o pipefail -o errexit
+
+if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
+    echo Usage: "$0 [test]"
+    exit 2
+fi
+
+# Usage: build-kg2-snakemake.sh [test]
+
+{
+echo "================= starting build-kg2-snakemake.sh =================="
+date
+
+CONFIG_DIR=`dirname "$0"`
+source ${CONFIG_DIR}/master-config.shinc
+
+BUILD_FLAG=${1-""}
+
+if [[ "${BUILD_FLAG}" == "test" ]]
+then
+    TEST_ARG="test"
+    TEST_ARG_D="-test"
+    TEST_ARG_DD="--test"
+else
+    TEST_ARG=""
+    TEST_ARG_D=""
+    TEST_ARG_DD=""
+fi
+
+# create symbolic link for the Snakefile in root directory
+cd ~
+ln -sf ~/RTX/code/kg2/Snakefile ~/Snakefile
+
+# activate the virtualenv and start running snakemake
+source ${VENV_DIR}/bin/activate
+snakemake -R Finish -j --config test="${TEST_ARG}" testd="${TEST_ARG_D}" testdd="${TEST_ARG_DD}"
+
+date
+echo "================ script finished ============================"
+} >~/snakemake.log 2>&1
