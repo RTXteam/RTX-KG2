4d24105b87b617d4722390605334d53f02c5453d
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-08-29 21:20:32 -0700

    #349

--- install-neo4j.sh
+++ install-neo4j.sh
@@ -11,6 +11,18 @@ fi
 
 sudo apt-get update -y
 sudo apt-get install -y emacs
+
+# install Oracle Java 8
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer
+sudo apt-get install oracle-java8-set-default
+sudo cat >> /etc/environment <<EOL
+JAVA_HOME=/usr/lib/jvm/java-8-oracle
+JRE_HOME=/usr/lib/jvm/java-8-oracle/jre
+EOL
+
+# install Neo4j
 wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
 echo 'deb http://debian.neo4j.org/repo stable/' > /tmp/neo4j.list
 sudo mv /tmp/neo4j.list /etc/apt/sources.list.d/neo4j.list
