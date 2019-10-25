42727e9703b657c1c3d5c4d89f96b1e6d043dc76
finnagin <flan313@gmail.com>
2019-10-24 21:49:44 -0700

    #546

---
+++ download-repodb-csv.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# build-kg2.sh:  script for downloading and importing repoDB 
+
+set -o nounset -o pipefail -o errexit
+
+if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
+    echo Usage: "$0 <output_tsv_dir>"
+    exit 2
+fi
+
+echo "================= starting doenload-repodb-csv.sh ================="
+date
+
+## load the master config file
+CONFIG_DIR=`dirname "$0"`
+source ${CONFIG_DIR}/master-config.shinc
+
+REPODB_DIR=${1:-"${BUILD_DIR}/repodb/"}
+REPODB_FILE=repodb.csv
+
+mkdir -p ${REPODB_DIR}
+
+aws s3 cp --no-progress --region ${S3_REGION} s3://${S3_BUCKET}/${REPODB_CSV_FILE} ${REPODB_DIR}/${REPODB_FILE}
+
+date
+echo "================= script finished ================="
