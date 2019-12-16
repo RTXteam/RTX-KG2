5ed7d034793a06c766cc5313379a667decabdbea
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-12-16 10:37:11 -0800

    #592

---
+++ prompt_for_password_and_save_to_temp_file.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+''' Prompts the user to enter a password on standard input and saves the password to a temporary file. Prints the temp file name.
+
+    Usage: prompt_for_password_and_save_to_temp_file.py
+'''
+
+__author__ = 'Stephen Ramsey'
+__copyright__ = 'Oregon State University'
+__credits__ = ['Stephen Ramsey']
+__license__ = 'MIT'
+__version__ = '0.1.0'
+__maintainer__ = ''
+__email__ = ''
+__status__ = 'Prototype'
+
+import getpass
+import tempfile
+
+
+if __name__ == '__main__':
+    password = getpass.getpass("Please enter a password")
+    tempfile = tempfile.mkstemp()[1]
+    with open(tempfile, 'w') as output_file:
+        print(password, file=output_file)
+    print(tempfile)
