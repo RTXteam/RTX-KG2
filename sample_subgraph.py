2b89ea1121a2e347f86668fa36a01530ef8994f4
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2019-12-16 09:47:15 -0800

    #427

--- sample_subgraph.py
+++ sample_subgraph.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+'''sample_subgraph.py: sample a random subgraph from a KG2 JSON graph, in JSON format
+
+   Usage: run "sample_subgraph.py --help" to get usage information
+'''
 
 __author__ = 'Stephen Ramsey'
 __copyright__ = 'Oregon State University'
@@ -16,15 +20,15 @@ import kg2_util
 def make_arg_parser():
     arg_parser = argparse.ArgumentParser(description='sample_subgraph.py: sample a smaller subgraph of a KG in JSON format')
     arg_parser.add_argument('--test', dest='test', action="store_true", default=False)
-    arg_parser.add_argument('--inputFile', type=str, nargs=1)
-    arg_parser.add_argument('--outputFile', type=str, nargs=1)
+    arg_parser.add_argument('inputFile', type=str, nargs=1)
+    arg_parser.add_argument('outputFile', type=str, nargs=1)
     return arg_parser
 
 
 if __name__ == '__main__':
     args = make_arg_parser().parse_args()
-    input_file_name = args.inputFile[0]
-    output_file_name = args.outputFile[0]
+    input_file_name = args.inputFile
+    output_file_name = args.outputFile
     graph = kg2_util.load_json(input_file_name)
     nodes = [graph['nodes'][i] for i in range(0, len(graph['nodes']), 5)]
     nodes_id_set = set([node['id'] for node in nodes])
