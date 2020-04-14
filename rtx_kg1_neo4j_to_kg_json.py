1772ea1c0ec94d8ade686b1bd9977872ee3af858
Amy Glen <amykglen@gmail.com>
2020-04-13 19:32:32 -0600

    Add provided_by mapping for AQTLTrait prefix #705

--- rtx_kg1_neo4j_to_kg_json.py
+++ rtx_kg1_neo4j_to_kg_json.py
@@ -56,7 +56,8 @@ KG1_PROVIDED_BY_TO_KG2_IRIS = {
     'CL': 'http://purl.obolibrary.org/ontology/cl.owl',
     'KEGG': 'http://genome.jp/kegg',
     'CHEMBL.COMPOUND': 'https://www.ebi.ac.uk/chembl',
-    'NCBIGene': 'https://www.ncbi.nlm.nih.gov/gene'
+    'NCBIGene': 'https://www.ncbi.nlm.nih.gov/gene',
+    'AQTLTrait': 'http://purl.obolibrary.org/ontology/hp.obo'  # KG1 has a single AQTLTrait node, which has an HP uri
     }
 
 
