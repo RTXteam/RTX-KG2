a49c7227dbfacfb9a7df19cc65df81a0fe377064
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2020-04-25 17:09:23 -0700

    #725

--- uniprotkb_dat_to_json.py
+++ uniprotkb_dat_to_json.py
@@ -24,7 +24,7 @@ RE_ORGANISM_TAXID = re.compile('NCBI_TaxID=(\d+)')
 FIELD_CODES_USE_STRING = ['ID', 'SQ', 'RA', 'RX', 'RT', 'KW', 'CC', 'GN']
 FIELD_CODES_DO_NOT_STRIP_NEWLINE = ['SQ']
 REGEX_PUBLICATIONS = re.compile('((?:(?:PMID)|(?:PubMed)):\d+)')
-REGEX_GENE_NAME = re.compile('^Name=([^ \;]+)(\;| )')
+REGEX_GENE_NAME = re.compile('^Name=([^ \;]+)')
 REGEX_GENE_SYNONYMS = re.compile('Synonyms=([^\;]+)')
 REGEX_HGNC = re.compile('^HGNC; (HGNC:\d+)')
 REGEX_NCBIGeneID = re.compile('^GeneID; (\d+)')
@@ -48,6 +48,7 @@ def parse_records_from_uniprot_dat(uniprot_dat_file_name: str,
             fields = line_str.split("   ", maxsplit=1)
             field_code = fields[0]
             if field_code == "//\n":
+                # end of a record
                 organism_host_list = record.get('organism_host', None)
                 organism = record.get('organism', None)
                 assert organism is not None
@@ -59,13 +60,13 @@ def parse_records_from_uniprot_dat(uniprot_dat_file_name: str,
                 field_value = fields[1]
                 if field_code == "":
                     field_code = 'SQ'
-                    field_value = field_value.lstrip(' ')
+                    field_value = field_value.lstrip()
                 if field_code not in FIELD_CODES_DO_NOT_STRIP_NEWLINE:
-                    field_value = field_value.rstrip('\n')
+                    field_value = field_value.rstrip('\n').rstrip(';')
                 if record.get(field_code, None) is None:
                     if field_code not in FIELD_CODES_USE_STRING:
                         if field_code != 'AC':
-                            record[field_code] = [field_value]
+                            record[field_code] = [field_value.rstrip(';')]
                         else:
                             record[field_code] = [ac.strip() for ac in field_value.split(';')]
                     else:
@@ -73,11 +74,11 @@ def parse_records_from_uniprot_dat(uniprot_dat_file_name: str,
                 else:
                     if type(record[field_code]) == list:
                         if field_code != 'AC':
-                            record[field_code].append(field_value)
+                            record[field_code].append(field_value.rstrip(';'))
                         else:
                             record[field_code] += [ac.strip() for ac in field_value.split(';')]
                     else:
-                        record[field_code] += field_value
+                        record[field_code] += field_value.lstrip()
                 if field_code == 'OH':
                     re_match = RE_ORGANISM_TAXID.match(field_value)
                     if re_match is not None:
@@ -174,10 +175,13 @@ def make_nodes(records: list):
                         name = next_desc.replace('Short=', '')
                         continue
             else:
-                if not description_str.startswith('Flags:'):
-                    synonyms.append(description_str)
+                if description_str.startswith('AltName: Full='):
+                    synonyms.append(description_str.replace('AltName: Full=', ''))
                 else:
-                    description += '; ' + description_str
+                    if description_str.startswith('AltName: CD_antigen='):
+                        synonyms.append(description_str.replace('AltName: CD_antigen=', ''))
+                    if not description_str.startswith('Flags:') and not description_str.startswith('Contains:'):
+                        description += '; ' + description_str
             desc_ctr += 1
         date_fields = record_dict['DT']
         date_ctr = 0
@@ -200,16 +204,18 @@ def make_nodes(records: list):
         publications += [pub.replace('PubMed:', 'PMID:') for pub in REGEX_PUBLICATIONS.findall(description)]
         publications = list(set(publications))
         gene_names_str = record_dict.get('GN', None)
+        gene_symbol = None
         if gene_names_str is not None:
-            gene_names_match = REGEX_GENE_NAME.match(gene_names_str)
-            if gene_names_match is not None:
-                gene_symbol = gene_names_match[1]
-                synonyms.append(gene_symbol)
-            else:
-                gene_symbol = None
-            gene_synonyms_match = REGEX_GENE_SYNONYMS.match(gene_names_str)
-            if gene_synonyms_match is not None:
-                synonyms += [syn.strip() for syn in gene_synonyms_match[1].split(',')]
+            gene_names_str_list = gene_names_str.split(';')
+            for gene_names_str_item in gene_names_str_list:
+                gene_names_match = REGEX_GENE_NAME.match(gene_names_str_item)
+                if gene_names_match is not None:
+                    gene_symbol = gene_names_match[1]
+                    synonyms.insert(0, gene_symbol)
+                else:
+                    gene_synonyms_match = REGEX_GENE_SYNONYMS.match(gene_names_str)
+                    if gene_synonyms_match is not None:
+                        synonyms += [syn.strip() for syn in gene_synonyms_match[1].split(',')]
         if name is None:
             if gene_symbol is not None:
                 name = gene_symbol
