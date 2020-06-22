92139f0ca28b15ad889a285d4212d7ca8386cc36
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2020-06-21 22:36:35 -0700

    #520 #747 #777

--- repodb_csv_to_kg_json.py
+++ repodb_csv_to_kg_json.py
@@ -16,12 +16,12 @@ import argparse
 import kg2_util
 import pandas as pd
 
-DRUGBANK_CURIE = "DRUGBANK:"
-UMLS_CURIE = "CUI:"
+DRUGBANK_CURIE = kg2_util.CURIE_PREFIX_DRUGBANK
+UMLS_CURIE = kg2_util.CURIE_PREFIX_CUI
 CLINICALTRIALS_IRI = "https://clinicaltrials.gov/ct2/show/"
-REPODB_IRI = "http://apps.chiragjpgroup.org/repoDB/"
-REPODB_CURIE = "REPODB:"
-NCT_CUTRIE = "clinicaltrials:"
+REPODB_IRI = kg2_util.BASE_URL_REPODB
+REPODB_CURIE = kg2_util.CURIE_PREFIX_REPODB
+NCT_CUTRIE = kg2_util.CURIE_PREFIX_CLINICALTRIALS
 
 
 def get_args():
@@ -42,23 +42,24 @@ def make_kg2_graph(input_file_name: str, test_mode: bool = False):
         else:
             status = "unknown_status"
         if not df['phase'].isna()[idx]:
-            phase = df['phase'][idx].lower().replace(" ", "_").replace("/","_or_")
+            phase = df['phase'][idx].lower().replace(" ", "_").replace("/", "_or_")
         else:
             phase = "unknown_phase"
         relation = "clinically_tested_" + status + "_" + phase
-        edge_dict = kg2_util.make_edge(subject_id = DRUGBANK_CURIE + df['drug_id'][idx],
-              object_id = UMLS_CURIE + df['ind_id'][idx],
-              relation = REPODB_IRI + '#' + kg2_util.convert_snake_case_to_camel_case(relation),
-              relation_curie = REPODB_CURIE + relation,
-              predicate_label = relation,
-              provided_by = REPODB_IRI,
-              update_date = None)
+        edge_dict = kg2_util.make_edge(subject_id=DRUGBANK_CURIE + df['drug_id'][idx],
+                                       object_id=UMLS_CURIE + df['ind_id'][idx],
+                                       relation=REPODB_IRI + '#' + kg2_util.convert_snake_case_to_camel_case(relation),
+                                       relation_curie=REPODB_CURIE + relation,
+                                       predicate_label=relation,
+                                       provided_by=REPODB_CURIE + ':',
+                                       update_date=None)
         if not df['NCT'].isna()[idx]:
             edge_dict['publications'].append(NCT_CUTRIE + df['NCT'][idx])
             edge_dict['publications info'][NCT_CUTRIE + df['NCT'][idx]] = CLINICALTRIALS_IRI + df['NCT'][idx]
         edges.append(edge_dict)
     return {'nodes': nodes,
             'edges': edges}
+
 
 if __name__ == '__main__':
     args = get_args()
@@ -67,5 +68,3 @@ if __name__ == '__main__':
     test_mode = args.test
     graph = make_kg2_graph(input_file_name, test_mode)
     kg2_util.save_json(graph, output_file_name, test_mode)
-
-
