33afd7970a734d4176b3eaf2ee5a15ce2fbf540d
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2020-07-03 14:28:21 -0700

    #864

--- install-neo4j.sh
+++ install-neo4j.sh
@@ -15,7 +15,7 @@ sudo apt-get install -y emacs
 # install Zulu OpenJDK Java
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
 sudo apt-add-repository 'deb http://repos.azulsystems.com/ubuntu stable main'
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y zulu-8
 
 # install Neo4j
