0280f64882150c859652845383dbf363071fd400
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2020-07-09 13:49:06 -0700

    #864

--- run-validation-tests.sh
+++ run-validation-tests.sh
@@ -18,22 +18,31 @@ echo "================= starting run-validation-tests.sh ================="
 date
 
 BIOLINK_MODEL_OWL_FILE=${BUILD_DIR}/biolink-model.owl
+BIOLINK_CONTEXT_JSONLD_FILE=${BUILD_DIR}/context.jsonld
+
 ${CURL_GET} https://raw.githubusercontent.com/biolink/biolink-model/master/biolink-model.owl > ${BIOLINK_MODEL_OWL_FILE}
-${CURL_GET} https://raw.githubusercontent.com/biolink/biolink-model/master/context.jsonld > ${BUILD_DIR}/context.jsonld
+${CURL_GET} https://raw.githubusercontent.com/biolink/biolink-model/master/context.jsonld > ${BIOLINK_CONTEXT_JSONLD_FILE}
+
 ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/validate_curies_to_categories_yaml.py \
            ${CURIES_TO_CATEGORIES_FILE} \
-           ${CURIES_TO_URLS_FILE}
+           ${CURIES_TO_URLS_FILE} \
+           ${BIOLINK_MODEL_OWL_FILE}
+
 ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/validate_curies_to_urls_map_yaml.py \
            ${CURIES_TO_URLS_FILE} \
-           ${BUILD_DIR}/context.jsonld
+           ${BIOLINK_CONTEXT_JSONLD_FILE}
+
 ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/validate_rtx_kg1_curie_mappings.py \
            ${CURIES_TO_URLS_FILE}
+
 ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/validate_kg2_util_curies_urls_categories.py \
            ${BIOLINK_MODEL_OWL_FILE} \
            ${CURIES_TO_URLS_FILE}
+
 ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/validate_predicate_remap_yaml.py \
            ${CURIES_TO_URLS_FILE} \
            ${PREDICATE_MAPPING_FILE}
+
 ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/validate_owl_load_inventory.py \
            ${OWL_LOAD_INVENTORY_FILE} \
            ${CURIES_TO_URLS_FILE} \
