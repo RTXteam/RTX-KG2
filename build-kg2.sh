153a3d21eeb9e4c6a75d656a03d3c084ba7e9e63
Erica Wood <36611732+ericawood@users.noreply.github.com>
2020-07-21 09:57:43 -0700

    #486

--- build-kg2.sh
+++ build-kg2.sh
@@ -306,6 +306,7 @@ ${VENV_DIR}/bin/python3 -u ${CODE_DIR}/merge_graphs.py \
                      ${REPODB_OUTPUT_FILE} \
 		     ${SMPDB_OUTPUT_FILE} \
 		     ${DRUGBANK_OUTPUT_FILE} \
+		     ${HMDB_OUTPUT_FILE} \
                      ${KG1_OUTPUT_FILE} \
            --kgFileOrphanEdges ${OUTPUT_FILE_ORPHAN_EDGES} \
            ${FINAL_OUTPUT_FILE_FULL}
