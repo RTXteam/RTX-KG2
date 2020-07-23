abcd8bf26b70a028d6b235c9a566a5831707da8e
Amy Glen <amykglen@gmail.com>
2020-07-23 13:34:19 -0700

    Simplify KG2C shell script a bit #944

--- canonicalized/build-kg2-canonicalized.sh
+++ canonicalized/build-kg2-canonicalized.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
-CONFIG_DIR=../`dirname "$0"`
-source ${CONFIG_DIR}/master-config.shinc
-
-SLIM_KG2_FILE_NAME = kg2-slim.json.gz
-CANONICALIZED_KG2_FILE_NAME = kg2-canonicalized.json
+slim_kg2_file_name = kg2-slim.json.gz
+canonicalized_kg2_file_name = kg2-canonicalized.json
 
 # Grab the latest "slim" KG2 from S3
-${S3_CP_CMD} s3://${S3_BUCKET}/{SLIM_KG2_FILE_NAME}
+aws s3 cp --no-progress --region us-west-2 s3://rtx-kg2/{slim_kg2_file_name}
 
 # Create the canonicalized KG from the slim KG (represented as JSON)
-${VENV_DIR}/bin/python3 -u create_canonicalized_kg_json.py ${SLIM_KG2_FILE_NAME} ${CANONICALIZED_KG2_FILE_NAME}
+python3 -u create_canonicalized_kg_json.py ${slim_kg2_file_name} ${canonicalized_kg2_file_name}
 
 # Convert the json file into TSV files TODO
-#TSV_DIR = canonicalized_tsvs
-#rm -r -f ${TSV_DIR}
-#mkdir -p ${TSV_DIR}
-#${VENV_DIR}/bin/python3 -u kg_canonicalized_json_to_tsv.py \
-#           kg2-canonicalized.json \
-#           ${TSV_DIR}
+#tsv_dir = canonicalized_tsvs
+#rm -r -f ${tsv_dir}
+#mkdir -p ${tsv_dir}
+#python3 -u kg_canonicalized_json_to_tsv.py ${canonicalized_kg2_file_name} ${tsv_dir}
 #tar -czvf kg2-canonicalized-tsv.tar.gz nodes_c.tsv nodes_c_header.tsv edges_c.tsv edges_c_header.tsv
-#${S3_CP_CMD} kg2-canonicalized-tsv.tar.gz s3://${S3_BUCKET}/
+#aws s3 cp --no-progress --region us-west-2 kg2-canonicalized-tsv.tar.gz s3://rtx-kg2/
