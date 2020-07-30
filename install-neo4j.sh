1e409ce2c2ae28cf8119a01c305daeff651d7257
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2020-07-30 10:54:47 -0700

    #983

--- install-neo4j.sh
+++ install-neo4j.sh
@@ -10,7 +10,7 @@ if [[ $# != 0 || "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
 fi
 
 sudo apt-get update
-sudo apt-get install -y emacs
+sudo apt-get install -y software-properties-common wget
 
 # install Zulu OpenJDK Java
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0xB1998361219BD9C9
@@ -30,22 +30,18 @@ sudo cp /etc/neo4j/neo4j.conf /etc/neo4j/neo4j.conf.ori
 
 # configure Neo4j for external network access
 cat /etc/neo4j/neo4j.conf | sed 's/#dbms.connectors.default_listen_address/dbms.connectors.default_listen_address/g' > /tmp/neo4j.conf
-sudo service neo4j stop
 sudo mv /tmp/neo4j.conf /etc/neo4j
-sudo service neo4j start
 
 # install APOC
 cd /tmp
 wget https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.5.0.4/apoc-3.5.0.4-all.jar
 wget https://github.com/neo4j-contrib/neo4j-graph-algorithms/releases/download/3.5.4.0/graph-algorithms-algo-3.5.4.0.jar
-sudo service neo4j stop
 sudo mv /tmp/graph-algorithms-algo-3.5.4.0.jar /var/lib/neo4j/plugins
 sudo mv /tmp/apoc-3.5.0.4-all.jar /var/lib/neo4j/plugins
 cat /etc/neo4j/neo4j.conf | sed 's/#dbms.security.procedures.unrestricted=my.extensions.example,my.procedures.*/dbms.security.procedures.unrestricted=apoc.*,algo.*/g' > \
                                 /tmp/neo4j.conf
 sudo mv /tmp/neo4j.conf /etc/neo4j
-sudo service neo4j start
 
-echo "Now you need to go to http://<hostname>:7474 in your browser and set a default password for the database"
-echo "Or you can do it on the command-line: sudo su - neo4j -c neo4j-admin set-initial-password <thepassword>"
+echo "Not starting Neo4j so that you can configure its initial password using neo4j-admin set-initial-password <PWD>"
+
 
