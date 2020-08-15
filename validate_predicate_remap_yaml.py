3b506d9213990dfedc9cd74266346b2c1cdc6cf2
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2020-08-15 15:05:27 -0700

    #1015

--- validate_predicate_remap_yaml.py
+++ validate_predicate_remap_yaml.py
@@ -16,6 +16,7 @@ __status__ = 'Prototype'
 
 import argparse
 import kg2_util
+
 import yaml
 
 
@@ -23,55 +24,76 @@ def make_arg_parser():
     arg_parser = argparse.ArgumentParser(description='validate_predicate_remap_yaml.py: checks the file `predicate-remap.yaml` for correctness.')
     arg_parser.add_argument('curiesToURLsMapFile', type=str)
     arg_parser.add_argument('predicateRemapFile', type=str)
-    arg_parser.add_argument('biolinkModelURL', type=str)
-    arg_parser.add_argument('biolinkModelLocalFile', type=str)
+    arg_parser.add_argument('biolinkModelYamlURL', type=str)
+    arg_parser.add_argument('biolinkModelYamlLocalFile', type=str)
     return arg_parser
 
-
-CURIE_PREFIXES_FOR_NUMBERED_RELATION_TYPES = {kg2_util.CURIE_PREFIX_RO,
-                                              kg2_util.CURIE_PREFIX_NCIT}
-CURIE_PREFIXES_FOR_CAMELCASE_RELATION_TYPES = {kg2_util.CURIE_PREFIX_SKOS}
 
 args = make_arg_parser().parse_args()
 curies_to_urls_map_file_name = args.curiesToURLsMapFile
 predicate_remap_file_name = args.predicateRemapFile
-biolink_model_url = args.biolinkModelURL
-biolink_model_file_name = args.biolinkModelLocalFile
+biolink_model_url = args.biolinkModelYamlURL
+biolink_model_file_name = args.biolinkModelYamlLocalFile
 
 curies_to_url_map_data = kg2_util.safe_load_yaml_from_string(kg2_util.read_file_to_string(curies_to_urls_map_file_name))
 curies_to_url_map_data_bidir = {next(iter(listitem.keys())) for listitem in curies_to_url_map_data['use_for_bidirectional_mapping']}
 
 kg2_util.download_file_if_not_exist_locally(biolink_model_url, biolink_model_file_name)
-biolink_ont = kg2_util.make_ontology_from_local_file(biolink_model_file_name)
-biolink_categories_ontology_depths = kg2_util.get_biolink_categories_ontology_depths(biolink_ont)
 
-biolink_edge_labels = {url.replace(kg2_util.BASE_URL_BIOLINK_META, '') for url in
-                       biolink_ont.children(kg2_util.BASE_URL_BIOLINK_META + 'SlotDefinition')}
+biolink_model = kg2_util.safe_load_yaml_from_string(kg2_util.read_file_to_string(biolink_model_file_name))
 
-map_data = yaml.safe_load(open(predicate_remap_file_name, 'r'))
-for relation_curie, instructions_dict in map_data.items():
-    for instruction, instructions_list in instructions_dict.items():
-        new_edge_label = None
-        if instruction == 'keep':
-            relation_curie_to_check = relation_curie
-            assert instructions_list is None, relation_curie
-        elif instruction == 'delete':
-            continue
+biolink_to_external_mappings = {'biolink:' + relation.replace(' ', '_'):
+                                list(map(lambda x: x.lower(), relation_info.get('mappings', []))) for
+                                relation, relation_info in biolink_model['slots'].items()}
+for relation, relation_info in biolink_model['slots'].items():
+    inverted_relation = relation_info.get('inverse', None)
+    mappings = relation_info.get('mappings', None)
+    if inverted_relation is not None and mappings is not None:
+        biolink_curie = 'biolink:' + inverted_relation.replace(' ', '_')
+        existing_list = biolink_to_external_mappings.get(biolink_curie, None)
+        if existing_list is None:
+            existing_list = []
+        existing_list += list(map(lambda x: x.lower(), mappings))
+        biolink_to_external_mappings[biolink_curie] = existing_list
+
+biolink_to_external_mappings['skos:closeMatch'] = []
+
+external_to_biolink_mappings = dict()
+for biolink_curie, external_curies in biolink_to_external_mappings.items():
+    for external_curie in external_curies:
+        if external_to_biolink_mappings.get(external_curie, None) is None:
+            external_to_biolink_mappings[external_curie] = set()
+        external_to_biolink_mappings[external_curie].add(biolink_curie)
+
+pred_info = yaml.safe_load(open(predicate_remap_file_name, 'r'))
+
+for relation, instruction_dict in pred_info.items():
+    command, subinfo = next(iter(instruction_dict.items()))
+    if command == 'keep' and not relation.startswith('biolink:'):
+        if not relation.startswith('BSPO:') and \
+           not relation.startswith('FMA:') and \
+           not relation.startswith('UBERON:'):
+            assert False, relation
         else:
-            new_edge_label, relation_curie_to_check = instructions_list
-            assert relation_curie_to_check != relation_curie, relation_curie
-        curie_prefix, curie_suffix = relation_curie_to_check.split(':')
-        assert curie_prefix in curies_to_url_map_data_bidir, relation_curie_to_check
-        if new_edge_label is not None and new_edge_label != curie_suffix and \
-           (curie_prefix not in CURIE_PREFIXES_FOR_NUMBERED_RELATION_TYPES and
-            (curie_prefix not in CURIE_PREFIXES_FOR_CAMELCASE_RELATION_TYPES or
-             kg2_util.convert_snake_case_to_camel_case(new_edge_label) != curie_suffix)):
-            print("CURIE suffix mismatch with edge label: " + str(instructions_dict))
-        if relation_curie_to_check.startswith(kg2_util.CURIE_PREFIX_BIOLINK + ':'):
-            edge_label = relation_curie_to_check.replace(kg2_util.CURIE_PREFIX_BIOLINK + ':', '')
-            assert edge_label in biolink_edge_labels, relation_curie
-            if instructions_list is not None:
-                assert edge_label == instructions_list[0], relation_curie
+            command = 'rename'
+            subinfo = ['coexists_with', 'biolink:coexists_with']
+    if subinfo is not None:
+        simplified_relation = subinfo[1]
+        if not simplified_relation.startswith('biolink:') and not simplified_relation.startswith('skos:closeMatch'):
+            if simplified_relation.startswith('FMA:') or \
+               simplified_relation.startswith('BSPO:') or \
+               simplified_relation.startswith('UBERON:'):
+                command = 'rename'
+                subinfo = ['coexists_with', 'biolink:coexists_with']
             else:
-                assert instruction == 'keep', relation_curie
-
+                assert False
+    if subinfo is not None:
+        assert subinfo[1] in biolink_to_external_mappings, relation
+        allowed_biolink_curies_set = external_to_biolink_mappings.get(relation.lower(), None)
+        if allowed_biolink_curies_set is not None:
+            assert subinfo[1] in allowed_biolink_curies_set, relation
+    else:
+        assert command == 'keep' or command == 'delete'
+        if command == 'keep':
+            assert relation in biolink_to_external_mappings, relation
+            assert relation.startswith('biolink:') or relation.startswith('skos:')
