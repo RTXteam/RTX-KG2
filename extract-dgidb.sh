49910af61f546da18e56868a6796754f22ffe3b5
Lindsey Kvarfordt <kvarforl@oregonstate.edu>
2020-10-06 12:59:29 -0700

    #1091 updated url in extract-dgidb.sh

--- extract-dgidb.sh
+++ extract-dgidb.sh
@@ -17,11 +17,17 @@ source ${config_dir}/master-config.shinc
 
 dgidb_dir=${1:-"${BUILD_DIR}/dgidb/"}
 dgidb_file=interactions.tsv
-
+dgidb_base_url="https://www.dgidb.org/"
 mkdir -p ${dgidb_dir}
 
-${curl_get} http://www.dgidb.org/data/${dgidb_file} > /tmp/${dgidb_file}
-update_date=`${curl_get} http://www.dgidb.org/downloads | grep 'Last updated' | sed 's/Last updated //g'`
+# not the most future proof, but finds the first table entry of interactions.tsv and grabs url from href
+dgidb_path=`${curl_get} http://www.dgidb.org/downloads | grep -m 1 'interactions.tsv' | sed 's:<td><a href="\(.*\)">.*</a></td>:\1:'`
+update_date=`echo ${dgidb_path} | grep -i -o -E '[0-9]{4}-[a-z]{3}'`
+dgidb_url="${dgidb_base_url}${dgidb_path}"
+
+${curl_get} ${dgidb_url} > /tmp/${dgidb_file}
+
+echo ${update_date}
 echo "#${update_date}" > ${dgidb_dir}/${dgidb_file}
 cat /tmp/${dgidb_file} >> ${dgidb_dir}/${dgidb_file}
 
