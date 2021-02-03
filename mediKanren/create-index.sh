10d10523f5fa9d8d582a8726bd5d33cb1c42b474
kvarforl <ubuntu@ip-172-31-59-26.us-west-2.compute.internal>
2021-02-03 19:32:42 +0000

    fixed pop conflict

--- mediKanren/create-index.sh
+++ mediKanren/create-index.sh
@@ -6,6 +6,8 @@ set -o nounset -o pipefail -o errexit
 
 cd mediKanren/biolink
 racket csv-graph-to-db.rkt data rtx_kg2
+echo "======finished csv-graph-to-db.racket======"
 racket build-string-index.rkt data rtx_kg2
+echo "======script finished====="
 
 
