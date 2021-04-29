e992a1519140fb4acb1d0888a2c4b459ebc0742f
amykglen <glena@oregonstate.edu>
2021-04-29 11:12:35 -0700

    Clear SRI cache in synonymizer build script

--- canonicalized/build-synonymizer.sh
+++ canonicalized/build-synonymizer.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # This script builds an ARAX NodeSynonymizer off of the KG2 version pointed to in your configv2.json file.
-# Usage: build-synonymizer.sh
+# Usage: bash -x build-synonymizer.sh
 
 set -e
 
@@ -9,6 +9,7 @@ arax_dir=${script_dir}/../../ARAX
 
 # Build a NodeSynonymizer using the KG2 endpoint specified under the "KG2" slot in RTX/code/configv2.json
 cd ${arax_dir}/NodeSynonymizer
+rm sri_node_normalizer_requests_cache.sqlite  # Cache may be stale, so we delete
 python3 -u dump_kg2_node_data.py
 python3 -u sri_node_normalizer.py --build
 python3 -u node_synonymizer.py --build
