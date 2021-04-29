eddce3821fc2b289894b34120b73749fb39a3447
amykglen <glena@oregonstate.edu>
2021-04-29 11:21:37 -0700

    Don't exit if fail to remove sri cache

--- canonicalized/build-synonymizer.sh
+++ canonicalized/build-synonymizer.sh
@@ -9,7 +9,7 @@ arax_dir=${script_dir}/../../ARAX
 
 # Build a NodeSynonymizer using the KG2 endpoint specified under the "KG2" slot in RTX/code/configv2.json
 cd ${arax_dir}/NodeSynonymizer
-rm sri_node_normalizer_requests_cache.sqlite  # Cache may be stale, so we delete
+rm -f sri_node_normalizer_requests_cache.sqlite  # Cache may be stale, so we delete
 python3 -u dump_kg2_node_data.py
 python3 -u sri_node_normalizer.py --build
 python3 -u node_synonymizer.py --build
