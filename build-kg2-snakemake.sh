fefe39020bf9ea5f49803fd5e5652c5f79932aa8
ericawood <ericacwood@gmail.com>
2021-06-15 15:12:55 -0700

    #71 travis CI debugging

--- build-kg2-snakemake.sh
+++ build-kg2-snakemake.sh
@@ -80,6 +80,7 @@ then
     sed -i "/\        simplified_output_nodes_file_full = config\['SIMPLIFIED_OUTPUT_NODES_FILE_FULL'\],/d" ${CODE_DIR}/Snakefile-finish
     sed -i '/\        shell("gzip -fk {input.simplified_output_nodes_file_full}")/d' ${CODE_DIR}/Snakefile-finish
     sed -i "/\        shell(config\['S3_CP_CMD'\] + ' {simplified_output_nodes_file_full}.gz s3:\/\/' + config\['S3_BUCKET'\])/d" ${CODE_DIR}/Snakefile-finish
+    cat ${CODE_DIR}/Snakefile-post-etl
 else
         git fetch origin
         git checkout origin/issue71 -- ${CODE_DIR}/Snakefile-post-etl
