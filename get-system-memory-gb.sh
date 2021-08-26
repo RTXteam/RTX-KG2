ffd228bc05755a114bce73abad68049c5862c06c
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2021-08-26 13:45:14 -0700

    #137

--- get-system-memory-gb.sh
+++ get-system-memory-gb.sh
@@ -10,7 +10,18 @@ if [[ $# != 0 || "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
 fi
 
 ## estimate the amount of system ram, in GB (Linux-only script)
-mem_bytes=`cat /proc/meminfo | grep MemTotal | cut -f2 -d\: | cut -f1 -dk | sed 's/ //g'`
+if [[ $OSTYPE == 'linux-gnu' ]];
+then
+    mem_bytes=`cat /proc/meminfo | grep MemTotal | cut -f2 -d\: | cut -f1 -dk | sed 's/ //g'`
+else
+    if [[ $OSTYPE == 'darwin'* ]];
+    then
+        mem_bytes=`sysctl -a | grep hw.memsize | cut -f2 -d\ `
+    else
+        echo "unsupported OS type for get-system-memory-gb.sh"
+        exit 1
+    fi
+fi
 divisor=1048576
 mem_gb=$((mem_bytes/divisor))
 echo ${mem_gb}
