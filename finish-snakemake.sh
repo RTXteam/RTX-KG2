3424feb5161d491e7c93fe6b15b7ab7410760180
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2021-08-30 16:46:45 -0700

    #144

--- finish-snakemake.sh
+++ finish-snakemake.sh
@@ -15,7 +15,7 @@
 if [[ "${1:-}" == "--help" || "${1:-}" == "-h" ]]; then
     echo Usage: "$0 [final_output_file_full] [output_file_orphan_edges] [report_file_full] [simplified_output_file_full] [simplified_report_file_full]"
     echo "[slim_output_file_full] [kg2_tsv_dir] [s3_cp_cmd]"
-    echo "[kg2_tsv_tarball] [s3_bucket] [s3_bucket_public] [output_file_base] [ont_load_inventory_file]"
+    echo "[kg2_tsv_tarball] [s3_bucket] [s3_bucket_public] [output_file_base] "
     echo "[CODE_DIR] [s3_bucket_versioned] [BUILD_DIR] [simplified_report_file_base] [VENV_DIR]"
     exit 2
 fi
@@ -32,12 +32,11 @@ kg2_tsv_tarball=${9}
 s3_bucket=${10}
 s3_bucket_public=${11}
 output_file_base=${12}
-ont_load_inventory_file=${13}
-CODE_DIR=${14}
-s3_bucket_versioned=${15}
-BUILD_DIR=${16}
-simplified_report_file_base=${17}
-VENV_DIR=${18}
+CODE_DIR=${13}
+s3_bucket_versioned=${14}
+BUILD_DIR=${15}
+simplified_report_file_base=${16}
+VENV_DIR=${17}
 previous_simplified_report_base="previous-${simplified_report_file_base}"
 
 echo "================= starting finish-snakemake.sh =================="
@@ -72,7 +71,6 @@ ${s3_cp_cmd} ${slim_output_file_full}.gz s3://${s3_bucket}/
 build_multi_owl_stderr_file="${BUILD_DIR}/build-${output_file_base%.*}"-stderr.log
 
 ${s3_cp_cmd} ${build_multi_owl_stderr_file} s3://${s3_bucket_public}/
-${s3_cp_cmd} ${ont_load_inventory_file} s3://${s3_bucket_public}/
 ${s3_cp_cmd} ${CODE_DIR}/s3-index.html s3://${s3_bucket_public}/index.html
 
 ${s3_cp_cmd} ${report_file_full} s3://${s3_bucket_versioned}/
