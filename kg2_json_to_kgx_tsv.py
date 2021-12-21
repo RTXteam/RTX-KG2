80819294533d8844b567091d0bb497b4276ec63e
Stephen Ramsey <stephen.ramsey@oregonstate.edu>
2021-12-21 10:23:31 -0800

    #154 added outputFile, and tab-checking

--- kg2_json_to_kgx_tsv.py
+++ kg2_json_to_kgx_tsv.py
@@ -20,16 +20,25 @@ __maintainer__ = ''
 __email__ = ''
 __status__ = 'Prototype'
 
+
+def check_tab(the_string: str,
+              line_ctr: int):
+    assert '\t' not in the_string, "tab found on line " + str(line_ctr + 1) + \
+        "; string: " + the_string
+    return the_string
+
+
 def make_arg_parser():
     arg_parser = argparse.ArgumentParser(description='kg2_json_to_kgx_tsv.py: load a RTX-KG2pre graph in JSON format and export it to KGX TSV format')
     arg_parser.add_argument('--logFile', type=str, default='kg2_json_to_kgx_tsv.log')
     arg_parser.add_argument('inputFile', type=str)
+    arg_parser.add_argument('outputDir', type=str, nargs='?', default='.')
     return arg_parser
 
     
-max_edges = None  ## Set to 100 for a test run or None for a full build
+max_rows = None  ## Set to 100 for a test run or None for a full build
 
-if max_edges is None:
+if max_rows is None:
     node_ids_to_select = None
 else:
     edge_counter = 0
@@ -38,6 +47,8 @@ else:
 args = make_arg_parser().parse_args()
 log_file_name = args.logFile
 input_file_name = args.inputFile
+output_dir_name = args.outputDir
+input_file_name_full = os.path.join(output_dir_name, input_file_name)
 
 # Clear any old output files
 if os.path.exists('edges.tsv'):
@@ -48,7 +59,7 @@ if os.path.exists('nodes.tsv'):
     os.remove('nodes.tsv')
 
 # Read the input file kg2-simplified.json
-with open(input_file_name, "r") as input_file:
+with open(input_file_name_full, "r") as input_file:
     input_data = json.load(input_file)
     input_file.close()
 
@@ -68,27 +79,28 @@ with open(log_file_name, 'a') as log_file:
 
     for item in edge_fields:
         if edge_fields.index(item) == len(edge_fields)-1:
-            output_file.write(item+'\n')
+            output_file.write(item + '\n')
         else:
-            output_file.write(item+'\t')
+            output_file.write(item + '\t')
     print("--- Headers written to edges.tsv ---", file=log_file)
 
     # Write edges.tsv
     num_edges = 0
     edges_data = input_data['edges']
     for item in edges_data:
-        if max_edges is None or num_edges < max_edges:
-            output_file.write(item['subject']+'\t')
-            output_file.write(item['object']+'\t')
-            output_file.write(item['predicate'] + '\t')
+        if max_rows is None or num_edges < max_rows:
+            output_file.write(check_tab(item['subject'], num_edges) + '\t')
+            output_file.write(check_tab(item['object'], num_edges) + '\t')
+            output_file.write(check_tab(item['predicate'], num_edges) + '\t')
             # knowledge source is a list
-            output_file.write('|'.join(item['knowledge_source']) + '\t')
+            ks_list = [check_tab(ks, num_edges) for ks in item['knowledge_source']]
+            output_file.write('|'.join(ks_list) + '\t')
             # publications is a list
-            output_file.write('|'.join(item['publications']) + '\t')
+            pubs_list = [check_tab(pub, num_edges) for pub in item['publications']]
+            output_file.write('|'.join(pubs_list) + '\t')
             # publications_info is a dictionary
             output_file.write(json.dumps(item['publications_info']) + '\t')
-#            output_file.write('|'.join(item['publications_info']) + '\t')
-            output_file.write(item['id'] + '\n')
+            output_file.write(check_tab(item['id'], num_edges) + '\n')
         num_edges += 1
 
     output_file.close()
@@ -110,18 +122,24 @@ with open(log_file_name, 'a') as log_file:
     nodes_data = input_data['nodes']
 
     # Write nodes.tsv
-    num_edges = 0
+    num_nodes = 0
     nodes_data = input_data['nodes']
     for item in nodes_data:
-        if max_edges is None or num_edges < max_edges:
-            output_file.write(item['id']+'\t')
-            output_file.write(str(item['name'])+'\t')
-            output_file.write(item['category'] + '\t')
-            output_file.write(item['iri'] + '\t')
-            output_file.write(item['description'] + '\t')
+        if max_rows is None or num_nodes < max_rows:
+            output_file.write(check_tab(item['id'], num_nodes) + '\t')
+            output_file.write(check_tab(str(item['name']), num_nodes) + '\t')
+            output_file.write(check_tab(item['category'], num_nodes) + '\t')
+            output_file.write(check_tab(item['iri'], num_nodes) + '\t')
+            desc = item['description']
+            if desc is not None:
+                check_tab(desc, num_nodes)
+            else:
+                desc = ''
+            output_file.write(desc + '\t')
             # publications is a list
-            output_file.write('|'.join(item['publications']) + '\n')
-        num_edges += 1
+            pub_list = [check_tab(publication, num_nodes) for publication in item['publications']]
+            output_file.write('|'.join(pub_list) + '\n')
+        num_nodes += 1
 
     output_file.close()
     print("--- Nodes.tsv completed --- ", file=log_file)
