12f4b4889f363b24fd39ed2c0a9259ff274d7537
acevedol <59181061+acevedol@users.noreply.github.com>
2022-08-09 09:09:15 -0700

    #221 Annotate filter_kg_and_remap_predicates.py

--- filter_kg_and_remap_predicates.py
+++ filter_kg_and_remap_predicates.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''Filters the RTX "KG2" second-generation knowledge graph, simplifying predicates and removing redundant edges.
+   Filter out negated edges, merge edges, remap predicates, remap source curies.
 
    Usage: filter_kg_and_remap_predicates.py <predicate-remap.yaml> <kg-input.json> <kg-output.json>
 '''
@@ -58,21 +59,26 @@ if __name__ == '__main__':
     if drop_self_edges_except is not None:
         assert type(drop_self_edges_except) == str
         drop_self_edges_except = set(drop_self_edges_except.split(','))
+    # Config files
     predicate_remap_config = kg2_util.safe_load_yaml_from_string(kg2_util.read_file_to_string(predicate_remap_file_name))
     infores_remap_config = kg2_util.safe_load_yaml_from_string(kg2_util.read_file_to_string(infores_remap_file_name))
     map_dict = kg2_util.make_uri_curie_mappers(curies_to_uri_file_name)
+    # ** Not sure about this notation ** 
     [curie_to_uri_expander, uri_to_curie_shortener] = [map_dict['expand'], map_dict['contract']]
     graph = kg2_util.load_json(input_file_name)
     new_edges = dict()
+    # Create sets to handle knowledge sources
     original_predicate_curies_not_in_config = set()
     provided_by_curies_not_in_config_nodes = set()
     provided_by_curies_not_in_config_edges = set()
     record_of_original_predicate_curie_occurrences = {original_predicate_curie: False for original_predicate_curie in
                                             predicate_remap_config.keys()}
+    # To Do: Update command set
     command_set = {'delete', 'keep', 'invert', 'rename'}
     for original_predicate_curie, command in predicate_remap_config.items():
         assert len(command) == 1
         assert next(iter(command.keys())) in command_set
+    # Every predicate curie should get its own node. Otherwise add to this set to complain.
     original_predicate_curies_not_in_nodes = set()
     nodes_dict = dict()
     for node_dict in graph['nodes']:
@@ -96,6 +102,7 @@ if __name__ == '__main__':
         predicate_label = original_predicate_label
         original_predicate_curie = edge_dict['original_predicate']
         predicate_curie = original_predicate_curie
+        # ** Double check meaning ** 
         if record_of_original_predicate_curie_occurrences.get(original_predicate_curie, None) is not None:
             record_of_original_predicate_curie_occurrences[original_predicate_curie] = True
             pred_remap_info = predicate_remap_config.get(original_predicate_curie, None)
@@ -123,23 +130,28 @@ if __name__ == '__main__':
                     get_new_rel_info = True
         if get_new_rel_info:
             predicate_label = remap_subinfo[0]
-            predicate_curie = remap_subinfo[1]
+            predicate_curie = remap_subinfo[1]  # Gets the biolink predicate
         if invert:
             edge_dict['relation_label'] = 'INVERTED:' + original_predicate_label
             new_object = edge_dict['subject']
             edge_dict['subject'] = edge_dict['object']
             edge_dict['object'] = new_object
         edge_dict['predicate_label'] = predicate_label
+        # Delete negated edges and self edges except interacts_with,positively_regulates,inhibits,increase
+        # as defined in run-simplify.sh
+        # To Do: verify exception list complies with updates from Biolink 3.0
         if drop_self_edges_except is not None and \
            edge_dict['subject'] == edge_dict['object'] and \
            predicate_label not in drop_self_edges_except:
             continue  # see issue 743
         edge_dict['predicate'] = predicate_curie
+        # Sets biolink curie; remapping
         if predicate_curie not in nodes_dict:
             predicate_curie_prefix = predicate_curie.split(':')[0]
             predicate_uri_prefix = curie_to_uri_expander(predicate_curie_prefix + ':')
+            # Create list of curies to complain about if not in biolink
             if predicate_uri_prefix == predicate_curie_prefix:
-                original_predicate_curies_not_in_nodes.add(predicate_curie)
+                original_predicate_curies_not_in_nodes.add(predicate_curie) 
         provided_by = edge_dict['knowledge_source']
         infores_curie_dict = infores_remap_config.get(provided_by, None)
         if infores_curie_dict is None:
@@ -147,20 +159,24 @@ if __name__ == '__main__':
         else:
             infores_curie = infores_curie_dict['infores_curie']
         edge_dict['knowledge_source'] = [infores_curie]
+        # TODO: Update with additional properties
         edge_key = edge_dict['subject'] + ' /// ' + predicate_label + ' /// ' + edge_dict['object']
         existing_edge = new_edges.get(edge_key, None)
+        # TODO: Update merging to handle semantic distinction between qualifiers
         if existing_edge is not None:
             existing_edge['knowledge_source'] = sorted(list(set(existing_edge['knowledge_source'] + edge_dict['knowledge_source'])))
             existing_edge['publications'] += edge_dict['publications']
             existing_edge['publications_info'].update(edge_dict['publications_info'])
         else:
             new_edges[edge_key] = edge_dict
+    # Releasing some memory
     del graph['edges']
     del graph['nodes']
     graph['nodes'] = list(nodes_dict.values())
     del nodes_dict
     graph['edges'] = list(new_edges.values())
     del new_edges
+    # Warnings for issues that came up
     for original_predicate_curie in record_of_original_predicate_curie_occurrences:
         if not record_of_original_predicate_curie_occurrences[original_predicate_curie]:
             print('original predicate curie is in the config file but was not used in any edge in the graph: ' + original_predicate_curie, file=sys.stderr)
@@ -191,6 +207,7 @@ if __name__ == '__main__':
     update_date = datetime.now().strftime("%Y-%m-%d %H:%M")
     version_file = open(args.versionFile, 'r')
     build_name = str
+    # Add node to describe build
     for line in version_file:
         test_flag = ""
         if test_mode:
