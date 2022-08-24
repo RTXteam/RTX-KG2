6cf5f9cd6c7997644c9a141843fddada61a7b89f
acevedol <59181061+acevedol@users.noreply.github.com>
2022-08-24 11:09:42 -0700

    #230

--- query_kegg.py
+++ query_kegg.py
@@ -74,7 +74,7 @@ def process_get_query(get_results, results_dict, kegg_id):
 
 def run_queries():
     results_dict = {}
-    info_queries = ["http://rest.kegg.jp/info"]
+    info_queries = ["http://rest.kegg.jp/info/kegg/"]
     list_queries = ["http://rest.kegg.jp/list/pathway/hsa",
                     "http://rest.kegg.jp/list/compound",
                     "http://rest.kegg.jp/list/glycan",
