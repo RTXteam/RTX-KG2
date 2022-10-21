60e457b47cc26ca57330aedb8c1ffdb1a38baed0
acevedol <59181061+acevedol@users.noreply.github.com>
2022-10-21 10:33:25 -0700

    #243 Updated to streaming

--- slim_kg2.py
+++ slim_kg2.py
@@ -3,9 +3,9 @@
     Usage: slim_kg2.py <inputFile>
                          --outputFile <outputFile
 '''
-__author__ = 'Lindsey Kvarfordt'
+__author__ = 'Liliana Acevedo'
 __copyright__ = 'Oregon State University'
-__credits__ = ['Lindsey Kvarfordt', 'Stephen Ramsey']
+__credits__ = ['Liliana Acevedo', 'Lindsey Kvarfordt', 'Stephen Ramsey']
 __license__ = 'MIT'
 __version__ = '0.1.0'
 __maintainer__ = ''
@@ -26,17 +26,6 @@ def make_arg_parser():
     return arg_parser
 
 
-def tokenizer():
-    # Write a tokenizer to handle chunks of a json stream
-    pass
-
-
-def stateMachine():
-    # remember which part of the json object we're currently processing. 
-    # Start of object -> Field Name -> Start of array -> start of object -> validate and save record
-    pass
-
-
 if __name__ == "__main__":
     node_set = set(["name", "id", "full_name", "category", "knowledge_source"])
     edge_set = set(["core_predicate", "subject", "object", "predicate_label", "knowledge_source"])
@@ -49,57 +38,42 @@ if __name__ == "__main__":
     # reduced_nodes = {"nodes": []}
     # reduced_edges = {"edges": []}
     with open(args.inputFilepath, "r") as fp:
-        
-        # File is too big to use json.load()
-        #all_data = json.load(fp)
 
         for item in ijson.items(fp, "build"):
             reduced["build"] = item
 
+    with open(args.inputFilepath, "r") as fp:
+
         node_ctr = 0
         num_nodes = 11117653  # Snagging this number for kg2-report.json for debugging purposes
-        for node in ijson.items(fp, "nodes"):
-            node_ctr += 1
-            if node_ctr % 1000000 == 0:
-                print(f"Processing node {str(node_ctr)} of {str(num_nodes)}")
-            temp_node = {}
-            for key, val in node:
-                if key in node_set:
-                    temp_node[key] = val
-            reduced["nodes"].append(temp_node)
-            print(temp_node)
+        
+        for node_list in ijson.items(fp, "nodes"):
+            for node in node_list:
+                node_ctr += 1
+                if node_ctr % 1000000 == 0:
+                    print(f"Processing node {str(node_ctr)} of {str(num_nodes)}")
+                temp_node = {}
+                for key, val in node.items():
+                    if key in node_set:
+                        temp_node[key] = val
+                reduced["nodes"].append(temp_node)
         print("Nodes completed")
 
-    #     reduced["build"] = all_data["build"]
-    #     num_nodes = len(all_data["nodes"])
-    #     node_ctr = 0
-    #     for node in all_data["nodes"]:
-    #         node_ctr += 1
-    #         if node_ctr % 1000000 == 0:
-    #             print(f"Processing node {str(node_ctr)} of {str(num_nodes)}")
-    #         temp_node = {}
-    #         for key, val in node.items():
-    #             if key in node_set:
-    #                 temp_node[key] = val
-    #         reduced["nodes"].append(temp_node)
-    #     print(f"Nodes completed")
+    with open(args.inputFilepath, "r") as fp:
 
-    #     num_nodes = len(all_data["edges"])
-    #     edge_ctr = 0
-    #     num_edges = len(all_data["edges"])
-    #     for edge in all_data["edges"]:
-    #         edge_ctr += 0
-    #         if edge_ctr % 1000000 ==0:
-    #             print(f"Processing edge {str(edge_ctr)} of {str(num_edges)}")
-    #         temp_edge = {}
-    #         if edge_ctr < 5:
-    #             print(f"Example edge: {edge}")
-    #         for key, val in edge.items():
-    #             if key in edge_set:
-    #                 temp_edge[key] = val
-    #             else:
-    #                 print(f"Key {key} not in edge set")
-    #         reduced["edges"].append(temp_edge)
-    #     print(f"Edges completed")
+        edge_ctr = 0
+        num_edges = 11117653  # Snagging this number for kg2-report.json for debugging purposes
+        
+        for edge_list in ijson.items(fp, "edges"):
+            for edge in edge_list:
+                edge_ctr += 1
+                if edge_ctr % 1000000 == 0:
+                    print(f"Processing edge {str(edge_ctr)} of {str(num_edges)}")
+                temp_edge = {}
+                for key, val in edge.items():
+                    if key in edge_set:
+                        temp_edge[key] = val
+                reduced["edges"].append(temp_edge)
+        print("Edges completed")
 
-    # kg2_util.save_json(reduced, args.outputFilepath, test_mode)
+    kg2_util.save_json(reduced, args.outputFilepath, test_mode)
