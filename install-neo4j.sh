56139c7c94c610ff1ebe8138e0f4380a51c43284
Stephen Ramsey <ramseyst@oregonstate.edu>
2023-04-11 17:42:16 +0000

    Updates

--- install-neo4j.sh
+++ install-neo4j.sh
@@ -19,8 +19,8 @@ sudo apt-get update
 sudo apt-get install -y zulu-8
 
 # install Neo4j
-wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
-echo 'deb http://debian.neo4j.org/repo stable/' > /tmp/neo4j.list
+wget --no-check-certificate -O - https://debian.neo4j.com/neotechnology.gpg.key | sudo apt-key add -
+echo 'deb http://debian.neo4j.com stable latest' > /tmp/neo4j.list
 sudo mv /tmp/neo4j.list /etc/apt/sources.list.d/neo4j.list
 sudo apt-get update
 sudo apt-get install -y neo4j=1:3.5.13
