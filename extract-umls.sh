73f503c953a8b291f8f013d9864a00f246621548
E.C. Wood <wooderi@stanford.edu>
2023-06-22 14:10:53 -0700

    #232 #291 issues with UMLS download and naming of folders on unzipping

--- extract-umls.sh
+++ extract-umls.sh
@@ -21,8 +21,8 @@ output_dir=${1:-${BUILD_DIR}}
 umls_cui_file=${2:-${BUILD_DIR}/umls_cuis.tsv}
 
 umls_ver=2022AA
-umls_file_base=${umls_ver}-metathesaurus
-umls2rdf_release=rtx-2.8
+umls_file_base=${umls_ver}-metathesaurus-buildkg2
+umls2rdf_release=rtx-2.2
 umls2rdf_pkgname=umls2rdf-${umls2rdf_release}
 umls2rdf_dir=${umls_dir}/${umls2rdf_pkgname}
 config_file=${umls_dir}/config.prop
@@ -46,7 +46,7 @@ cp ${CODE_DIR}/umls-config.prop ${config_file}
 ## unpack UMLS zip archive
 unzip ${umls_dir}/umls-${umls_file_base}.zip -d ${umls_dir}/
 
-mv ${umls_dir}/${umls_ver}/META/* ${umls_dest_dir} 
+mv ${umls_dir}/umls-${umls_ver}-metathesaurus.zip/META/* ${umls_dest_dir} 
 
 mysql_user=`grep 'user = ' ${mysql_conf} | sed 's/user = //g'`
 mysql_password=`grep 'password = ' ${mysql_conf} | sed 's/password = //g'`
@@ -59,12 +59,16 @@ mysql --defaults-extra-file=${mysql_conf} \
 mysql --defaults-extra-file=${mysql_conf} \
       -e "CREATE DATABASE IF NOT EXISTS ${mysql_dbname} CHARACTER SET utf8 COLLATE utf8_unicode_ci"
 
+mysql --defaults-extra-file=${mysql_conf} \
+      -e "SET GLOBAL local_infile = true"
+
 ## fill in the authentication and database variables in the shell script for populating the mysql database
 cat ${umls_dest_dir}/populate_mysql_db.sh | \
     sed "s/<username>/${mysql_user}/g" | \
     sed 's|<path to MYSQL_HOME>|/usr|g' | \
     sed "s/<password>/${mysql_password}/g" | \
-    sed "s/<db_name>/${mysql_dbname}/g" > ${umls_dest_dir}/populate_mysql_db_configured.sh
+    sed "s/<db_name>/${mysql_dbname}/g" | \
+    sed "s/-vvv/-vvv --local-infile=1/g" > ${umls_dest_dir}/populate_mysql_db_configured.sh
 
 ## enable the loading script to be runnable
 chmod +x ${umls_dest_dir}/populate_mysql_db_configured.sh
