79b0dbbf68813e5a6d1aa237c3c24f3df52c17be
E.C. Wood <wooderi@stanford.edu>
2023-06-23 14:49:53 -0700

    #291 #292 #232 working UMLS extract for UMLS2023AA

--- extract-umls.sh
+++ extract-umls.sh
@@ -20,9 +20,9 @@ source ${config_dir}/master-config.shinc
 output_dir=${1:-${BUILD_DIR}}
 umls_cui_file=${2:-${BUILD_DIR}/umls_cuis.tsv}
 
-umls_ver=2022AA
-umls_file_base=${umls_ver}-metathesaurus-buildkg2
-umls2rdf_release=rtx-2.2
+umls_ver=2023AA
+umls_file_base=umls-${umls_ver}-metathesaurus-full
+umls2rdf_release=rtx-2.2 # This is the version of umls2rdf NOT RTX-KG2; do not change to update RTX-KG2 version
 umls2rdf_pkgname=umls2rdf-${umls2rdf_release}
 umls2rdf_dir=${umls_dir}/${umls2rdf_pkgname}
 config_file=${umls_dir}/config.prop
@@ -40,13 +40,13 @@ mkdir -p ${umls_dir}
 mkdir -p ${umls_dest_dir}
 
 ## copy UMLS distribution files and MetamorphoSys config files from S3 to local dir
-${s3_cp_cmd} s3://${s3_bucket}/umls-${umls_file_base}.zip ${umls_dir}/
+${s3_cp_cmd} s3://${s3_bucket}/${umls_file_base}.zip ${umls_dir}/
 cp ${CODE_DIR}/umls-config.prop ${config_file}
 
 ## unpack UMLS zip archive
-unzip ${umls_dir}/umls-${umls_file_base}.zip -d ${umls_dir}/
+unzip ${umls_dir}/${umls_file_base}.zip -d ${umls_dir}/
 
-mv ${umls_dir}/umls-${umls_ver}-metathesaurus.zip/META/* ${umls_dest_dir} 
+mv ${umls_dir}/${umls_ver}/META/* ${umls_dest_dir} 
 
 mysql_user=`grep 'user = ' ${mysql_conf} | sed 's/user = //g'`
 mysql_password=`grep 'password = ' ${mysql_conf} | sed 's/password = //g'`
