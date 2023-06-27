515fed97463df34bfc609ac9dcc975e2be05235d
E.C. Wood <wooderi@stanford.edu>
2023-06-27 10:06:34 -0700

    #295 making it so drugcentral extract has necessary permissions

--- extract-drugcentral.sh
+++ extract-drugcentral.sh
@@ -33,13 +33,14 @@ psql_dump_file="${drugcentral_dir}/psql_dump_file.txt"
 user="ubuntu"
 role="jjyang"
 
+cd ~postgres/
+
 ${curl_get} ${source} > ${download_filename}
 sudo -u postgres psql -c "DROP DATABASE IF EXISTS ${drugcentral_database}"
 sudo -u postgres psql -c "CREATE DATABASE ${drugcentral_database}"
 sudo -u postgres psql -c "DROP ROLE IF EXISTS ${role}"
 sudo -u postgres psql -c "CREATE ROLE ${role}"
-# gunzip -c ${download_filename} | sudo -u postgres psql -v ON_ERROR_STOP=1 ${drugcentral_database}
-gunzip -c ${download_filename} | psql ${drugcentral_database}
+gunzip -c ${download_filename} | sudo -u postgres psql -v ON_ERROR_STOP=1 ${drugcentral_database}
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE ${drugcentral_database} TO ${user}"
 sudo -u postgres psql -d ${drugcentral_database} -c "GRANT ALL PRIVILEGES ON SCHEMA public TO ${user}"
 sudo -u postgres psql -d ${drugcentral_database} -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ${user}"
