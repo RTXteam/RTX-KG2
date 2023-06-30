9aa895416f9ff7abbae5fb699869244ea908a2c0
E.C. Wood <wooderi@stanford.edu>
2023-06-30 09:38:41 -0700

    #300 build multi ont isn't failing now

--- multi_ont_to_json_kg.py
+++ multi_ont_to_json_kg.py
@@ -1135,7 +1135,7 @@ def get_rels_dict(nodes: dict,
             if xrefs is not None:
                 for xref_node_id in xrefs:
                     if xref_node_id in nodes and node_id != xref_node_id:
-                        provided_by = nodes[node_id]['provided_by']
+                        provided_by = nodes[node_id]['provided_by'][0]
                         key = make_rel_key(node_id, CURIE_OBO_XREF, xref_node_id, provided_by)
                         if rels_dict.get(key, None) is None:
                             edge = kg2_util.make_edge(node_id,
