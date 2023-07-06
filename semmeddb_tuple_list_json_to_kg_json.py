3ddd2ac8d9305fc456016ff8fa3f2858b1afd85d
E.C. Wood <wooderi@stanford.edu>
2023-07-06 14:48:33 -0700

    #310 making it less user error prone

--- semmeddb_tuple_list_json_to_kg_json.py
+++ semmeddb_tuple_list_json_to_kg_json.py
@@ -91,7 +91,7 @@ def make_rel(preds_dict: dict,
 def make_arg_parser():
     arg_parser = argparse.ArgumentParser(description='semmeddb_mysql_to_json.py: extracts all the predicate triples from SemMedDB, in the RTX KG2 JSON format')
     arg_parser.add_argument('--test', dest='test', action='store_true', default=False)
-    arg_parser.add_argument('--mrcuiFile', dest='mrcui_file_name', type=str, default=None)
+    arg_parser.add_argument('--mrcuiFile', dest='mrcui_file_name', type=str, default='/home/ubuntu/kg2-build/umls/META/MRCUI.RRF')
     arg_parser.add_argument('inputFile', type=str)
     arg_parser.add_argument('outputFile', type=str)
     return arg_parser
