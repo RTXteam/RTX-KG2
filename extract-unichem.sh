817f37fcf68078a2df4606e5081e779fd862e052
E.C. Wood <wooderi@stanford.edu>
2023-07-06 09:27:51 -0700

    #293 #291 updating to new FTP layout and removed versioning method since there are no version in unichem anymore

--- extract-unichem.sh
+++ extract-unichem.sh
@@ -17,17 +17,17 @@ source ${config_dir}/master-config.shinc
 output_tsv_file=${1:-"${BUILD_DIR}/unichem/unichem-mappings.tsv"}
 unichem_dir=${BUILD_DIR}/unichem
 unichem_output_dir=`dirname ${output_tsv_file}`
-unichem_ver=385
-unichem_ftp_site=ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data
+unichem_ftp_site=ftp://ftp.ebi.ac.uk/pub/databases/chembl/UniChem/data/table_dumps
+
+uc_xref_filename=reference.txt.gz
+uc_source_filename=source.txt.gz
 
 rm -r -f ${unichem_dir}
 mkdir -p ${unichem_dir}
 mkdir -p ${unichem_output_dir}
 
-${curl_get} ${unichem_ftp_site}/oracleDumps/UDRI${unichem_ver}/UC_XREF.txt.gz -o ${unichem_dir}/UC_XREF.txt.gz
-${curl_get} ${unichem_ftp_site}/oracleDumps/UDRI${unichem_ver}/UC_SOURCE.txt.gz -o ${unichem_dir}/UC_SOURCE.txt.gz
-${curl_get} ${unichem_ftp_site}/oracleDumps/UDRI${unichem_ver}/UC_RELEASE.txt.gz -o ${unichem_dir}/UC_RELEASE.txt.gz
-${curl_get} ${unichem_ftp_site}/oracleDumps/UDRI${unichem_ver}/README -o ${unichem_dir}/README.txt
+${curl_get} ${unichem_ftp_site}/${uc_xref_filename} -o ${unichem_dir}/${uc_xref_filename}
+${curl_get} ${unichem_ftp_site}/${uc_source_filename} -o ${unichem_dir}/${uc_source_filename}
 
 chembl_src_id=`zcat ${unichem_dir}/UC_SOURCE.txt.gz | awk '{if ($2 == "chembl") {printf "%s", $1}}'`
 chebi_src_id=`zcat ${unichem_dir}/UC_SOURCE.txt.gz | awk '{if ($2 == "chebi") {printf "%s", $1}}'`
@@ -36,17 +36,12 @@ kegg_src_id=`zcat ${unichem_dir}/UC_SOURCE.txt.gz | awk '{if ($2 == "kegg_ligand
 drugcentral_src_id=`zcat ${unichem_dir}/UC_SOURCE.txt.gz | awk '{if ($2 == "drugcentral") {printf "%s", $1}}'`
 hmdb_src_id=`zcat ${unichem_dir}/UC_SOURCE.txt.gz | awk '{if ($2 == "hmdb") {printf "%s", $1}}'`
 
-update_date=`zcat ${unichem_dir}/UC_RELEASE.txt.gz | tail -1 | cut -f3`
-version_number=`grep "This directory contains files exported from UniChem Release number" ${unichem_dir}/README.txt | cut -f10 -d ' '`
-echo "# ${update_date}" > ${output_tsv_file}
-echo "# ${version_number}" >> ${output_tsv_file}
-
-zcat ${unichem_dir}/UC_XREF.txt.gz | awk '{if ($2 == '${chebi_src_id}') {print $1 "\tCHEBI:" $3}}' | sort -k1 > ${unichem_dir}/chebi.txt
-zcat ${unichem_dir}/UC_XREF.txt.gz | awk '{if ($2 == '${chembl_src_id}') {print $1 "\tCHEMBL.COMPOUND:" $3}}' | sort -k1 > ${unichem_dir}/chembl.txt
-zcat ${unichem_dir}/UC_XREF.txt.gz | awk '{if ($2 == '${drugbank_src_id}') {print $1 "\tDRUGBANK:" $3}}' | sort -k1 > ${unichem_dir}/drugbank.txt
-zcat ${unichem_dir}/UC_XREF.txt.gz | awk '{if ($2 == '${kegg_src_id}') {print $1 "\tKEGG:" $3}}' | sort -k1 > ${unichem_dir}/kegg.txt
-zcat ${unichem_dir}/UC_XREF.txt.gz | awk '{if ($2 == '${drugcentral_src_id}') {print $1 "\tDrugCentral:" $3}}' | sort -k1 > ${unichem_dir}/drugcentral.txt
-zcat ${unichem_dir}/UC_XREF.txt.gz | awk '{if ($2 == '${hmdb_src_id}') {print $1 "\tHMDB:" $3}}' | sort -k1 > ${unichem_dir}/hmdb.txt
+zcat ${unichem_dir}/${uc_xref_filename} | awk '{if ($2 == '${chebi_src_id}') {print $1 "\tCHEBI:" $3}}' | sort -k1 > ${unichem_dir}/chebi.txt
+zcat ${unichem_dir}/${uc_xref_filename} | awk '{if ($2 == '${chembl_src_id}') {print $1 "\tCHEMBL.COMPOUND:" $3}}' | sort -k1 > ${unichem_dir}/chembl.txt
+zcat ${unichem_dir}/${uc_xref_filename} | awk '{if ($2 == '${drugbank_src_id}') {print $1 "\tDRUGBANK:" $3}}' | sort -k1 > ${unichem_dir}/drugbank.txt
+zcat ${unichem_dir}/${uc_xref_filename} | awk '{if ($2 == '${kegg_src_id}') {print $1 "\tKEGG:" $3}}' | sort -k1 > ${unichem_dir}/kegg.txt
+zcat ${unichem_dir}/${uc_xref_filename} | awk '{if ($2 == '${drugcentral_src_id}') {print $1 "\tDrugCentral:" $3}}' | sort -k1 > ${unichem_dir}/drugcentral.txt
+zcat ${unichem_dir}/${uc_xref_filename} | awk '{if ($2 == '${hmdb_src_id}') {print $1 "\tHMDB:" $3}}' | sort -k1 > ${unichem_dir}/hmdb.txt
 
 join ${unichem_dir}/chembl.txt ${unichem_dir}/chebi.txt | sed 's/ /\t/g' | cut -f2-3 >> ${output_tsv_file}
 join ${unichem_dir}/chembl.txt ${unichem_dir}/drugbank.txt | sed 's/ /\t/g' | cut -f2-3 >> ${output_tsv_file}
