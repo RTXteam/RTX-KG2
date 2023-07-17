4d7229b9ef097c488d7b48f4bf384f5f2ca5049d
E.C. Wood <wooderi@stanford.edu>
2023-07-17 09:38:49 -0700

    #281 addressing failed build try 2

--- setup-kg2-build.sh
+++ setup-kg2-build.sh
@@ -47,7 +47,7 @@ then
     trap "cat ${setup_log_file}" EXIT
 fi
 
-{
+# {
 echo "================= starting setup-kg2.sh ================="
 date
 
@@ -117,7 +117,7 @@ chmod +x ${BUILD_DIR}/robot
 ${curl_get} ${BUILD_DIR} https://github.com/RTXteam/owltools/releases/download/v0.3.0/owltools > ${BUILD_DIR}/owltools
 chmod +x ${BUILD_DIR}/owltools
 
-} >${setup_log_file} 2>&1
+# } >${setup_log_file} 2>&1
 
 if [[ "${build_flag}" != "ci" ]]
 then
@@ -129,7 +129,7 @@ then
     fi
 fi
 
-{
+# {
 RAPTOR_NAME=raptor2-2.0.15
 # setup raptor (used by the "checkOutputSyntax.sh" script in the umls2rdf package)
 ${curl_get} -o ${BUILD_DIR}/${RAPTOR_NAME}.tar.gz http://download.librdf.org/source/${RAPTOR_NAME}.tar.gz
@@ -178,7 +178,7 @@ fi
 date
 
 echo "================= script finished ================="
-} >> ${setup_log_file} 2>&1
+# } >> ${setup_log_file} 2>&1
 
 if [[ "${build_flag}" != "ci" ]]
 then
