9a37417e5c5ff23b15803b9649ac76692931b3b1
ecwood <wooderi@stanford.edu>
2023-07-20 11:57:44 -0700

    #321 working (and tested) merge graphs with JSON Lines

--- merge_graphs.py
+++ merge_graphs.py
@@ -26,7 +26,8 @@ def make_arg_parser():
     arg_parser = argparse.ArgumentParser(description='merge_graphs.py: merge two or more JSON KG files')
     arg_parser.add_argument('--test', dest='test', action="store_true", default=False)
     arg_parser.add_argument('--kgFileOrphanEdges', type=str, nargs='?', default=None)
-    arg_parser.add_argument('--outputFile', type=str, nargs='?', default=None)
+    arg_parser.add_argument('--outputNodesFile', type=str, nargs='?', default=None)
+    arg_parser.add_argument('--outputEdgesFile', type=str, nargs='?', default=None)
     arg_parser.add_argument('--kgNodesFiles', type=str, nargs='+')
     arg_parser.add_argument('--kgEdgesFiles', type=str, nargs='+')
     return arg_parser
@@ -55,16 +56,20 @@ if __name__ == '__main__':
                              ontology_name=kg_nodes_file_name,
                              output_stream=sys.stderr)
         num_nodes_added = 0
-        for node in kg2_util.read_json_lines(kg_nodes_file_name):
+        kg_nodes_read_jsonlines_info = kg2_util.start_read_jsonlines(kg_nodes_file_name)
+        kg_nodes = kg_nodes_read_jsonlines_info[0]
+        for node in kg_nodes:
             node_id = node['id']
             if node_id not in nodes:
                 nodes[node_id] = node
                 num_nodes_added += 1
             else:
                 nodes[node_id] = kg2_util.merge_two_dicts(nodes[node_id], node)
-        kg2_util.log_message("number of nodes added: " + num_nodes_added,
+        kg2_util.log_message("number of nodes added: " + str(num_nodes_added),
                              ontology_name=kg_nodes_file_name,
                              output_stream=sys.stderr)
+    kg2_util.end_read_jsonlines(kg_nodes_read_jsonlines_info)
+
     ctr_edges_added = 0
     last_edges_added = 0
     last_orphan_edges = 0
@@ -74,7 +79,11 @@ if __name__ == '__main__':
         kg2_util.log_message("reading edges from file",
                              ontology_name=kg_edges_file_name,
                              output_stream=sys.stderr)
-        for rel_dict in kg2_util.read_json_lines(kg_edges_file_name):
+
+        edges_read_jsonlines_info = kg2_util.start_read_jsonlines(kg_edges_file_name)
+        kg_edges = edges_read_jsonlines_info[0]
+
+        for rel_dict in kg_edges:
             subject_curie = rel_dict['subject']
             object_curie = rel_dict['object']
             if subject_curie in nodes and object_curie in nodes:
@@ -86,6 +95,8 @@ if __name__ == '__main__':
             else:
                 orphan_output.write(rel_dict)
                 kg_orphan_edges_count += 1
+
+        kg2_util.end_read_jsonlines(edges_read_jsonlines_info)
 
         kg2_util.log_message("number of edges added: " + str(ctr_edges_added - last_edges_added),
                              ontology_name=kg_edges_file_name,
