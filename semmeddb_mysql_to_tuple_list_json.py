b4537b29765350424cb1052224452dc1048c7ce6
ecwood <wooderi@stanford.edu>
2023-07-24 12:27:22 -0700

    #321 first attempt at condensed MySQL results for SemMedDB extraction

--- semmeddb_mysql_to_tuple_list_json.py
+++ semmeddb_mysql_to_tuple_list_json.py
@@ -36,18 +36,35 @@ def make_arg_parser():
 
 
 if __name__ == '__main__':
+    print("Starting semmeddb_mysql_to_tuple_list_json.py at", kg2_util.date())
     args = make_arg_parser().parse_args()
     mysql_config_file = args.mysqlConfigFile
     mysql_db_name = args.mysqlDBName
     version_number = args.versionNumber
     version_date = args.versionDate
+    output_file_name = args.outputFile
     version_number = version_number.strip('VER')
     test_mode = args.test
     connection = pymysql.connect(read_default_file=mysql_config_file, db=mysql_db_name)
     preds_dict = dict()
-    sql_statement = ("SELECT PMID, SUBJECT_CUI, SUBJECT_SEMTYPE, PREDICATE, OBJECT_CUI, OBJECT_SEMTYPE, DP, SENTENCE, SUBJECT_SCORE, "
-                     "OBJECT_SCORE, DATE_FORMAT(CURR_TIMESTAMP, '%Y-%m-%d %H:%i:%S') FROM ((PREDICATION NATURAL JOIN CITATIONS) "
-                     "NATURAL JOIN SENTENCE) NATURAL JOIN PREDICATION_AUX")
+    # sql_statement = ("SELECT SUBJECT_CUI, PREDICATE, OBJECT_CUI, PMID, SUBJECT_SEMTYPE, , , OBJECT_SEMTYPE, DP, SENTENCE, SUBJECT_SCORE, "
+    #                  "OBJECT_SCORE, DATE_FORMAT(CURR_TIMESTAMP, '%Y-%m-%d %H:%i:%S') FROM ((PREDICATION NATURAL JOIN CITATIONS) "
+    #                  "NATURAL JOIN SENTENCE) NATURAL JOIN PREDICATION_AUX GROUP BY SUBJECT_CUI, PREDICATE, OBJECT_CUI")
+
+    # https://stackoverflow.com/questions/7208773/mysql-row-30153-was-cut-by-group-concat-error
+
+    output_info = kg2_util.create_single_jsonlines(test_mode)
+    output = output_info[0]
+
+    max_len_sql_statement = "SET group_concat_max_len=15000"
+
+    sql_statement = ("SELECT SUBJECT_CUI, PREDICATE, OBJECT_CUI, GROUP_CONCAT(DISTINCT SUBJECT_SEMTYPE), GROUP_CONCAT(DISTINCT OBJECT_SEMTYPE), "
+                     "GROUP_CONCAT(DISTINCT DATE_FORMAT(CURR_TIMESTAMP, '%Y-%m-%d %H:%i:%S')), "
+                     "GROUP_CONCAT(CONCAT(PMID, '|', SENTENCE, '|', SUBJECT_SCORE, '|', OBJECT_SCORE, '|', DP) SEPARATOR '\t') "
+                     "FROM ((PREDICATION NATURAL JOIN CITATIONS) NATURAL JOIN SENTENCE) NATURAL JOIN PREDICATION_AUX "
+                     "GROUP BY SUBJECT_CUI, PREDICATE, OBJECT_CUI")
+
+
     if test_mode:
         sql_statement += " LIMIT 10000"
     results = {'data_dictionary': ['pmid',
@@ -56,15 +73,21 @@ if __name__ == '__main__':
                                    'predicate',
                                    'object_cui_str',
                                    'object_semtype',
-                                   'pub_date',
+                                   'pub_date', #DP
                                    'sentence',
                                    'subject_score',
                                    'object_score',
                                    'curr_timestamp']}
     results['versioning'] = {'version_number': version_number, 'version_date': version_date}
     with connection.cursor() as cursor:
+        cursor.execute(max_len_sql_statement)
+        cursor.fetchall()
+
+        # Execute statement we care about after clearing any "results"
         cursor.execute(sql_statement)
-        results['rows'] = cursor.fetchall()
+        for result in cursor.fetchall():
+            output.write(result)
     connection.close()
-    output_file_name = args.outputFile
-    kg2_util.save_json(results, output_file_name, test_mode)
+
+    kg2_util.close_single_jsonlines(orphan_info, output_file_name)
+    print("Finishing semmeddb_mysql_to_tuple_list_json.py at", kg2_util.date())
