e41f8e29a8fe2c56305cc116c6392515e56464f6
ecwood <wooderi@stanford.edu>
2023-07-26 16:24:16 -0700

    #321 improving the data tracker

--- primative-instance-data-tracker.sh
+++ primative-instance-data-tracker.sh
@@ -3,17 +3,16 @@
 # Copyright 2023 Stephen A. Ramsey <stephen.ramsey@oregonstate.edu>
 # Author Erica Wood
 
-start_time=`date +"%Y-%m-%d"`
+start_time=`date +"%Y-%m-%d-%H-%M-%S"`
 {
 echo "================= starting primative-instance-data-tracker.sh ================="
-date
 
 while [[ true ]]; do
-	date
+	current_time=`date +"%Y-%m-%d-%H-%M-%S"`
 
-	memory_data=`landscape-sysinfo | grep "Memory usage" | sed "s/Memory usage://" | sed "s/IPv4 address for ens5: .*//" | sed "s/ //"`
-
-	echo ${memory_data}
+	memory_data=`landscape-sysinfo | grep "Memory usage" | sed "s/Memory usage://" | sed "s/IPv4 address for ens5: .*//" | sed "s/^[ ]*//" | sed "s/[ ]*$//"`
+	disk_space_data=`landscape-sysinfo | grep "Usage of /" | sed "s/Usage of \/://" | sed "s/Users logged in: .*//" | sed "s/^[ ]*//"| sed "s/[ ]*$//"`
+	echo "Time: ${current_time}; Memory: ${memory_data}; Disk: ${disk_space_data}"
 
 	sleep 60
 done
