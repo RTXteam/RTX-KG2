3488401263dc4868ebca7d5bdf91ee851a7713de
ecwood <wooderi@stanford.edu>
2024-09-02 14:11:01 -0700

    #408 #398 curl problems

--- extract/extract-clinicaltrialskg.sh
+++ extract/extract-clinicaltrialskg.sh
@@ -24,7 +24,10 @@ version="2.2.6"
 clinicaltrialskg_download_link="https://db.systemsbiology.net/gestalt/KG/clinical_trials_kg_edges_v${version}.tsv"
 
 echo "# ${version}" > ${clinicaltrialskg_output_file}
-${curl_get} ${clinicaltrialskg_download_link} >> ${clinicaltrialskg_output_file}
+# ${curl_get} ${clinicaltrialskg_download_link} >> ${clinicaltrialskg_output_file}
+
+# Short term fix because download link is not resolving and I cannot identify the correct download link
+${aws_s3_cp} s3://${s3_bucket}/clinicaltrialskg-edges.tsv ${clinicaltrialskg_output_file}
 
 date
 echo "================= finishing extract-clinicaltrialskg.sh =================="
